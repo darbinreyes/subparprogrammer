5d1864f4f8b296fa839d76afaa8e604dfc1b0c5f
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-01-28 13:16:36 -0500

    Update README.md and euclid.c.

---
+++ C/euclid/README.md
@@ -0,0 +1,8 @@
+# Implementations of propositions from Euclid's Elements.
+
+I will use the following as a reference for the propositions:
+https://mathcs.clarku.edu/~djoyce/java/elements/toc.html
+
+TODO:
+* Implement VII.1.
+* Add test cases using seatest.
---
+++ C/euclid/euclid.c
@@ -0,0 +1,7 @@
+/** @file
+  C programs corresponding to propositions from Euclid's Elements.
+
+  Coding style: following EDK2 coding standards since I am already familiar with it.
+
+  Testing: Hopefully I can get "seatest" working for testing functions in this file.
+**/
