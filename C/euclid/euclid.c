a325b1d9fb93cf0f5785eff230000e0435c55cee
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-01-28 13:55:03 -0500

    Add implementation of VII_1_relatively_prime().

--- C/euclid/euclid.c
+++ C/euclid/euclid.c
@@ -49,10 +49,74 @@ swap (
   return;
 }
 
+/**
+
+  Determines if the given numbers are prime to one another,
+  a.k.a. the numbers have no common measure greater than 1.
+  For Euclid, the unit, 1, is not a number.
+  a.k.a. the numbers have no common divisor greater than 1.
+  a.k.a. the numbers are relatively prime.
+
+  @param  a The first number.
+  @param  b The second number.
+
+  @retval -1 If !(a > 1 && b > 1).This is considered invalid input.
+  @retval 1  If the numbers are prime to one another.
+  @retval 0  If the numbers have a common measure greater than 1.
+
+**/
+int
+VII_1_relatively_prime (
+  unsigned int a,
+  unsigned int b
+  )
+{
+  if (!( a > 1 && b > 1)){
+    assert(0);
+    return -1;
+  }
+
+  /**
+    Euclid's algorithm. This is called an "anteneresis process".
+
+    Continually subtract the smaller from the greater until an unit is left.
+    The basis of this algorithm depends on the observation that if a number
+    measures two given numbers, it also measures their difference.
+
+  **/
+
+  while (a > 1 && b > 1) {
+    if (b > a)
+      swap(&a, &b);
+
+    a = a - b;
+  }
+
+  return a;
+}
+
+/**
+
+  The main function.
+
+**/
 int
 main (void)
 {
   printf("Hello, Euclid.\n");
+
+  unsigned int a, b;
+  a = 12;
+  b = 4;
+  printf ("a = %u, b = %u. rp = %d. expect rp = 0.\n", a, b, VII_1_relatively_prime(a, b));
+
+  a = 13;
+  b = 4;
+  printf ("a = %u, b = %u. rp = %d. expect rp = 1.\n", a, b, VII_1_relatively_prime(a, b));
+
+  a = 54;
+  b = 85;
+  printf ("a = %u, b = %u. rp = %d. expect rp = 1.\n", a, b, VII_1_relatively_prime(a, b));
 
   return 0;
 }
