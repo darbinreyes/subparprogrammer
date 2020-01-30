62e6ad173a8e30de63e085c8e747818889438b4c
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-01-30 15:01:05 -0500

    Convert indent to spaces.

--- C/euclid/test_suite_two.c
+++ C/euclid/test_suite_two.c
@@ -85,26 +85,26 @@ void test_asserting()
   assert_true( VII_1_relatively_prime (13, 4) == 1);
   assert_true( VII_1_relatively_prime (54, 85) == 1);
 
-	assert_true( 1 == 1);
-	assert_false(1 == 2);
-	assert_int_equal(1, 1);
+  assert_true( 1 == 1);
+  assert_false(1 == 2);
+  assert_int_equal(1, 1);
 }
 
 void test_assert_fails()
 {
-	assert_true( 1 == 2);
-	assert_true( 3 == 5); // for checking END_TEST_IF_ASSERT_FAIL option (will be skipped)
+  assert_true( 1 == 2);
+  assert_true( 3 == 5); // for checking END_TEST_IF_ASSERT_FAIL option (will be skipped)
 }
 
 void test_fixture_two( void )
 {
 
-	test_fixture_start();
+  test_fixture_start();
   run_test(test_VII_1);
   run_test(test_VII_2);
-	run_test(test_asserting);
-	run_test(test_assert_fails);
-	test_fixture_end();	
+  run_test(test_asserting);
+  run_test(test_assert_fails);
+  test_fixture_end();
   /**
 
   der: At the end of a test suite "4 run  3 failed":
