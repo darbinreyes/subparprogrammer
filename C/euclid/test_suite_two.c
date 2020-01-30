c1dc63688d0337f4f19a3ce4802fae0e5355696f
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-01-30 14:57:17 -0500

    Fix makefile for other target. Add comments about seatest.

--- C/euclid/test_suite_two.c
+++ C/euclid/test_suite_two.c
@@ -98,10 +98,33 @@ void test_assert_fails()
 
 void test_fixture_two( void )
 {
+
 	test_fixture_start();
   run_test(test_VII_1);
   run_test(test_VII_2);
 	run_test(test_asserting);
 	run_test(test_assert_fails);
 	test_fixture_end();	
+  /**
+
+  der: At the end of a test suite "4 run  3 failed":
+  means
+  4 = number of calls to run_test() that were executed.
+  3 = number of asserts that failed e.g. assert_true(0) is 1 failure.
+
+  When compiled without "-D ABORT_TEST_IF_ASSERT_FAIL", all calls to run_test()
+  execute to completion.
+  Hence if the test is simply:
+  assert_true (0);
+  assert_true (0);
+  This will result is "2 failed"
+
+  When compiled with "-D ABORT_TEST_IF_ASSERT_FAIL"
+  a call to run_test(test_VII_1) will stop further execution of test_VII_1()
+  immediately after an assert fails. Hence if the test is simply:
+  assert_true (0);
+  assert_true (0);
+  This will result is "1 failed".
+
+  **/
 }
