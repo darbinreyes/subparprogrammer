188e3a0495caf60348052e4796db1798d2af086c
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-03-16 13:22:45 -0400

    Python 3 exercise progress.

--- python/python3tutorial/reasoning_problem2.py
+++ python/python3tutorial/reasoning_problem2.py
@@ -1,4 +1,20 @@
 # coding style : https://www.python.org/dev/peps/pep-0008/
 # recommended way implementing a C like struct.: https://docs.python.org/3/tutorial/classes.html#odds-and-ends
 # I will use matplotlib to draw points and lines. https://matplotlib.org/tutorials/introductory/usage.html#sphx-glr-tutorials-introductory-usage-py
+import random
 
+class coloredPoint:
+    def __init__(self, color, x, y):
+        self.color = color # 0 = red, 1 = blue.
+        self.x = x
+        self.y = y
+
+def main():
+    print("hello")
+    p0 = coloredPoint(0, 7, 5)
+    p1 = coloredPoint(0, 3, 1)
+    print (p0.x, p1.x)
+    # random.sample(range(xmin, xmax+1), n)
+
+if __name__ == '__main__':
+  main()
