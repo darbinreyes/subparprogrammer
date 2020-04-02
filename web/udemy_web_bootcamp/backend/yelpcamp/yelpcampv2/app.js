ae8c5334bd37e6645031589fc8eff49219625f41
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-04-02 13:10:34 -0400

    Create yelpcampv2 dir.

---
+++ web/udemy_web_bootcamp/backend/yelpcamp/yelpcampv2/app.js
@@ -0,0 +1,63 @@
+// Express is our HTTP server.
+const express = require("express");
+const app = express();
+
+app.use(express.static("public")); // Tell express where to look for CSS files.
+
+// We need body-parser for easy access to form data provided in the body of an POST 
+// request that is formatted in x-www-form-urlencoded
+const bodyParser = require("body-parser"); 
+
+// Enable x-www-form-urlencoded in body-parser. Tell express to use body-parser.
+app.use(bodyParser.urlencoded({extended: true})); 
+
+// Add a GET request handler for /.
+app.get("/", function (exp_request, exp_response) {
+    console.log("GET @ /.");
+    exp_response.render("landing.ejs");
+});
+
+// This is our fake database.
+nutritions = [
+{name: "Mango", image: "https://upload.wikimedia.org/wikipedia/commons/f/fb/Carabao_mangoes_%28Philippines%29.jpg"},
+{name: "Lime", image: "https://upload.wikimedia.org/wikipedia/commons/6/68/Lime-Whole-Split.jpg"},
+{name: "Strawberry", image: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Strawberries.jpg"},
+{name: "Avocado", image: "https://upload.wikimedia.org/wikipedia/commons/c/c0/Avocado-board.jpg"},
+{name: "Green Leaf Lettuce", image: "https://upload.wikimedia.org/wikipedia/commons/8/85/HK_food_%E8%94%AC%E8%8F%9C_vegetable_Lettuce_%E7%94%9F%E8%8F%9C_Lactuca_Sativa_green_leaves_with_fresh_clear_water_January_2019_SSG_02.jpg"},
+{name: "Tomato", image: "https://upload.wikimedia.org/wikipedia/commons/b/b7/Tomato-top.png"},
+{name: "Mango", image: "https://upload.wikimedia.org/wikipedia/commons/f/fb/Carabao_mangoes_%28Philippines%29.jpg"},
+{name: "Lime", image: "https://upload.wikimedia.org/wikipedia/commons/6/68/Lime-Whole-Split.jpg"},
+{name: "Strawberry", image: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Strawberries.jpg"},
+{name: "Avocado", image: "https://upload.wikimedia.org/wikipedia/commons/c/c0/Avocado-board.jpg"},
+{name: "Green Leaf Lettuce", image: "https://upload.wikimedia.org/wikipedia/commons/8/85/HK_food_%E8%94%AC%E8%8F%9C_vegetable_Lettuce_%E7%94%9F%E8%8F%9C_Lactuca_Sativa_green_leaves_with_fresh_clear_water_January_2019_SSG_02.jpg"},
+{name: "Tomato", image: "https://upload.wikimedia.org/wikipedia/commons/b/b7/Tomato-top.png"}
+];
+
+// Add a GET request handler for /.
+app.get("/campgrounds", function (exp_request, exp_response) {
+    console.log("GET @ /campgrounds.");
+    exp_response.render("campgrounds.ejs", {nutritions: nutritions});
+});
+
+app.post("/campgrounds", function (exp_request, exp_response){
+    console.log("POST @ /campgrounds.");
+    // get form data
+    // redirect to /campgrounds.
+    var newName = exp_request.body.name;
+    var newImage = exp_request.body.image;
+    nutritions.push({name: newName, image: newImage});
+    exp_response.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function (exp_request, exp_response) {
+    console.log("GET @ /campgrounds/new.");
+    exp_response.render("new.ejs");
+});
+
+const PORT = 3000;
+const IP = "127.0.0.1";
+
+// Tell express to start listening for HTTP requests.
+app.listen(PORT, IP, function(){
+    console.log("Server is listening! Droid remember go blank. ... blank blank!");
+});
