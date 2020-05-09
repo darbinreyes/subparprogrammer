efe02dd4ed7adc6003d4628d53493d58e9e346a5
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-05-09 18:32:51 -0400

    Update seedDB.js so that it creates a single User. Associate that user with the initial DB nutrition and comment entries. This fixes the issue of blank "Submitted by" and comment author upon seeding the DB..

--- web/udemy_web_bootcamp/backend/yelpcamp/yelpcampv10/src/seedDB.js
+++ web/udemy_web_bootcamp/backend/yelpcamp/yelpcampv10/src/seedDB.js
@@ -9,15 +9,28 @@ const description1 = "There are several species of citrus trees whose fruits are
 const description2 = "The garden strawberry (or simply strawberry; Fragaria × ananassa)[1] is a widely grown hybrid species of the genus Fragaria, collectively known as the strawberries, which are cultivated worldwide for their fruit. The fruit is widely appreciated for its characteristic aroma, bright red color, juicy texture, and sweetness. It is consumed in large quantities, either fresh or in such prepared foods as jam, juice, pies, ice cream, milkshakes, and chocolates. Artificial strawberry flavorings and aromas are also widely used in products such as candy, soap, lip gloss, perfume, and many others. The garden strawberry was first bred in Brittany, France, in the 1750s via a cross of Fragaria virginiana from eastern North America and Fragaria chiloensis, which was brought from Chile by Amédée-François Frézier in 1714.[2] Cultivars of Fragaria × ananassa have replaced, in commercial production, the woodland strawberry (Fragaria vesca), which was the first strawberry species cultivated in the early 17th century.[3]";
 
 const data = [
- {name: "Mango", image: "https://upload.wikimedia.org/wikipedia/commons/f/fb/Carabao_mangoes_%28Philippines%29.jpg", energy: 60, description: description0},
- {name: "Lime", image: "https://upload.wikimedia.org/wikipedia/commons/6/68/Lime-Whole-Split.jpg", energy: 30, description: description1},
- {name: "Strawberry", image: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Strawberries.jpg", energy: 32, description: description2}
+ {name: "Mango", image: "https://upload.wikimedia.org/wikipedia/commons/f/fb/Carabao_mangoes_%28Philippines%29.jpg", energy: 60, description: description0, author: {}},
+ {name: "Lime", image: "https://upload.wikimedia.org/wikipedia/commons/6/68/Lime-Whole-Split.jpg", energy: 30, description: description1, author: {}},
+ {name: "Strawberry", image: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Strawberries.jpg", energy: 32, description: description2, author: {}}
 ];
 
-function seedDB() {
-  console.log("Seeding the DB.");
+function RemoveAllDB() {
+  // Delete all users, nutrition entries, and comments from the DB.
+  const User = require("./models/user");
   const Nutrition = require("./models/nutrition");
   const Comment = require("./models/comment");
+
+  // Remove all DB User entries.
+  console.log("Removing all users.");
+  User.remove({}, function(err, removed_entries){
+    if(err) {
+        console.log(".remove() error: ");
+        console.log(err);
+    } else {
+        console.log(".remove() successful: ");
+        //console.log(removed_entries);
+    }
+  });
 
   // Remove all DB nutrition entries.
   console.log("Removing all nutritions.");
@@ -30,53 +43,85 @@ function seedDB() {
         //console.log(removed_entries);
     }
   });
-  // Remove all DB comment entries.
-  // console.log("Removing all comments.");
-  // Comment.remove({}, function(err, removed_entries){
-  //   if(err) {
-  //       console.log(".remove() error: ");
-  //       console.log(err);
-  //   } else {
-  //       console.log(".remove() successful: ");
-  //       //console.log(removed_entries);
-  //   }
-  // });
 
+  // Remove all DB comment entries.
+  console.log("Removing all comments.");
+  Comment.remove({}, function(err, removed_entries){
+    if(err) {
+        console.log(".remove() error: ");
+        console.log(err);
+    } else {
+        console.log(".remove() successful: ");
+        //console.log(removed_entries);
+    }
+  });
+}
+
+function AddNutritionsAndComments(registeredUser) {
+  // Add nutritions and comments associated with registeredUser.
+  const Nutrition = require("./models/nutrition");
+  const Comment = require("./models/comment");
+  var author = {id: registeredUser._id, username: registeredUser.username};
   // Add all DB nutrition entries from the data array.
-  // data.forEach(function(currentValue, index, array) {
-  //   console.log("Creating Nutrition Entry.");
-  //   Nutrition.create(currentValue, function(err, new_nutrition){
-  //     if(err) {
-  //         console.log(".create() error: ");
-  //         console.log(err);
-  //     } else {
-  //         console.log(".create() successful: ");
-  //         //console.log(new_nutrition);
-  //         var test_comment = {text: "Screw you guys, I'm goin' home.", author: "Eric Cartman"};
-  //         console.log("Creating Comment Entry.");
-  //         Comment.create(test_comment, function(err, new_comment){
-  //           if(err) {
-  //               console.log(".create() error: ");
-  //               console.log(err);
-  //           } else {
-  //               console.log(".create() successful: ");
-  //               //console.log(new_comment);
-  //               console.log(".pushing() comment and saving().");
-  //               new_nutrition.comments.push(new_comment);
-  //               new_nutrition.save(function(err, saved_nutrition){
-  //                 if(err) {
-  //                     console.log(".save() error: ");
-  //                     console.log(err);
-  //                 } else {
-  //                     console.log(".save() successful: ");
-  //                     //console.log(saved_nutrition);
-  //                 }
-  //               })
-  //           }
-  //         });
-  //     }
-  //   });
-  // });
+  data.forEach(function(currentValue, index, array) {
+    console.log("Creating Nutrition Entry.");
+    currentValue.author = author;
+    Nutrition.create(currentValue, function(err, new_nutrition){
+      if(err) {
+          console.log(".create() error: ");
+          console.log(err);
+      } else {
+          console.log(".create() successful: ");
+          console.log(new_nutrition);
+          var test_comment = {text: "Screw you guys, I'm goin' home.", author: author};
+          console.log("Creating Comment Entry.");
+          Comment.create(test_comment, function(err, new_comment){
+            if(err) {
+                console.log(".create() error: ");
+                console.log(err);
+            } else {
+                console.log(".create() successful: ");
+                console.log(".pushing() comment and saving().");
+                new_nutrition.comments.push(new_comment);
+                new_nutrition.save(function(err, saved_nutrition){
+                  if(err) {
+                      console.log(".save() error: ");
+                      console.log(err);
+                  } else {
+                      console.log(".save() successful: ");
+                  }
+                })
+            }
+          });
+      }
+    });
+  });
+}
+
+function seedDB() {
+  console.log("Seeding the DB.");
+
+  RemoveAllDB();
+
+  // DB is now in an empty state. A user must be created first since nutritions
+  // and comments have users associated with them.
+
+  // Register a single user.
+  const User = require("./models/user");
+  console.log("Registering a user.");
+  User.register(new User({username: "cartman"}), "123", function(err, registeredUser) {
+      if(err) {
+        console.log("User.register() error: ");
+        console.log(err);
+      } else {
+        // This is the "middleware" pattern ostensibly.
+        console.log("User.register() successful: ");
+        // We must add the remaining DB entries (non-User entries) inside this
+        // callback to ensure the user exists in the DB before referencing it in
+        // nutrition/comment associations.
+        AddNutritionsAndComments(registeredUser);
+      }
+    });
 }
 
 module.exports = seedDB;
