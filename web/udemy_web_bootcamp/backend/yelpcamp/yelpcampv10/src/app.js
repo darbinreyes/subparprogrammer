197a33cfe16d4d7cfb1d95426406d41cff23a23a
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-05-12 14:01:31 -0400

    Cleanup comments.

--- web/udemy_web_bootcamp/backend/yelpcamp/yelpcampv10/src/app.js
+++ web/udemy_web_bootcamp/backend/yelpcamp/yelpcampv10/src/app.js
@@ -13,18 +13,24 @@ function foo(x: ?number): string {
 // Express is our HTTP server.
 const express = require("express");
 
-/* We need body-parser for easy access to form data provided in the body of a
-POST request that is formatted in x-www-form-urlencoded */
+/*
+  We need body-parser for easy access to form data provided in the body of a
+  POST request that is formatted in x-www-form-urlencoded
+*/
 const bodyParser = require("body-parser");
 
 // Mongoose is our DB API based on the mongo DB.
 const mongoose = require("mongoose");
 
-// method-override allows us to work-around the fact that HTML forms only
-// support method=POST or GET.
+/*
+  method-override allows us to work-around the fact that HTML forms only
+  support method=POST or GET.
+*/
 const methodOverride = require('method-override');
 // We use express-sanitizer to remove e.g. script tags from user text inputs.
 const expressSanitizer = require("express-sanitizer");
+
+
 
 /************* Authentication related requires() START **/
 // Our main authentication package.
@@ -32,8 +38,10 @@ const passport = require("passport");
 // Passport's "local" authentication "strategy".
 const LocalStrategy = require("passport-local");
 
-/* Used by express for client session API (client state). Keeps track of when a
-user is currently logged in. */
+/*
+  Used by express for client session API (client state). Keeps track of when a
+  user is currently logged in.
+*/
 const expressSession = require("express-session");
 /************* Authentication related requires() END **/
 
@@ -43,8 +51,7 @@ const expressSession = require("express-session");
 // "yelpnutritionv10" will appear in "mongo shell>>> show dbs" list.
 mongoose.connect("mongodb://localhost/yelpnutritionv10", {useNewUrlParser: true, useUnifiedTopology: true});
 
-/* Deletes everything in the DB, except for users, then adds some initial DB
-entries. */
+// Deletes everything in the DB, then adds some initial DB entries.
 const seedDB = require("./seedDB");
 seedDB();
 
@@ -55,7 +62,7 @@ const User = require("./models/user"); // mongoose model for Users.
 
 /************* Express setup START **/
 const app = express();
-/* Tell express where to look for e.g. CSS files and images. */
+// Tell express where to look for e.g. CSS files and images.
 app.use(express.static("public"));
 
 app.use(expressSession( // Tell express to use express-session
@@ -68,66 +75,75 @@ app.use(expressSession( // Tell express to use express-session
 app.use(passport.initialize()); // Tell express to use passport step 1
 app.use(passport.session()); // Tell express to use passport step 2
 
-/* Enable x-www-form-urlencoded in body-parser. Tell express to use
-body-parser. */
+// Enable x-www-form-urlencoded in body-parser. Tell express to use body-parser.
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Tell Express to use the method-override package. This configuration of
-// method-override looks for a POST request with a URL query parameter
-// _method=PUT and turns that POST into a PUT request within Express.
+/*
+  Tell Express to use the method-override package. This configuration of
+  method-override looks for a POST request with a URL query parameter
+  _method=PUT and turns that POST into a PUT request within Express.
+*/
 app.use(methodOverride('_method'));
 // IMPORTANT: express-sanitizer app.use() should go AFTER body-parser above.
 app.use(expressSanitizer());
 /************* Express setup END **/
 
+
+
 /************* passport setup START **/
 
-/* This is a line added in as part of the POST @ /login to implement the user
-login function. See secret page code along pt. 4. Tell passport to "for the local strategy
-use that method for the User.authenticate()" */
+/*
+  This is a line added in as part of the POST @ /login to implement the user
+  login function. See secret page code along pt. 4. Tell passport to "for the
+  local strategy use that method for the User.authenticate()".
+*/
 passport.use(
   new LocalStrategy(User.authenticate())
   );
 
-/* "Two more lines we need to get setup and then we can start working on the
-routes". Secret page code along pt.2. User.serializeUser() comes from
-./models/user.js->UserSchema.plugin(passportLocalMongoose); */
+/*
+  "Two more lines we need to get setup and then we can start working on the
+  routes". Secret page code along pt.2. User.serializeUser() comes from
+  ./models/user.js->UserSchema.plugin(passportLocalMongoose);.
+*/
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 /************* passport setup END **/
 
 /*
-"our own little middleware" - this is makes it so that the object describing
-a user is passed in to all .ejs files. This is how we can implement
-hiding/showing of the login/sign-up/logout links in the navbar based on whether
-or not the user is logger in. It also allows us to display the name of the
-currently logged in user in the navbar. This is really express setup but I'm
-pretty sure this line must occur after passport setup. Without this we would
-have to pass req.user to exp_response.render() almost every time it is called in
-a route.*/
+  "our own little middleware" - this is makes it so that the object describing
+  a user is passed in to all .ejs files. This is how we can implement
+  hiding/showing of the login/sign-up/logout links in the navbar based on
+  whether or not the user is logger in. It also allows us to display the name of
+  the currently logged in user in the navbar. This is really express setup but
+  I'm pretty sure this line must occur after passport setup. Without this we
+  would have to pass req.user to exp_response.render() almost every time it is
+  called in a route.
+*/
 app.use(function(req, resp, next){
   resp.locals.currentUser = req.user; // See "currentUser" in header.ejs.
   next();
 });
 
-/* require the routes. */
+// require the routes.
 var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
-/* Instead of "app.use(commentRoutes);"" we can tell express to prefix the
-routes with the string provided in the first argument. This way we avoid errors
-caused by a mistyped route path string. Note that if a route parameter like
-":id" occurs in this prefix you must use "express.Router({mergeParams: true});".
-See routes/comments.js. */
+/*
+  Instead of "app.use(commentRoutes);" we can tell express to prefix the
+  routes with the string provided in the first argument. This way we avoid
+  errors caused by a mistyped route path string. Note that if a route parameter
+  like ":id" occurs in this prefix you must use
+  "express.Router({mergeParams: true});". See routes/comments.js.
+*/
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/", indexRoutes);
 
+// Tell express to start listening for HTTP requests.
 const PORT = 3000;
 const IP = "127.0.0.1";
-
-// Tell express to start listening for HTTP requests.
 app.listen(PORT, IP, function(){
     console.log("Server is listening! Droid remember go blank. ... blank blank!");
 });
