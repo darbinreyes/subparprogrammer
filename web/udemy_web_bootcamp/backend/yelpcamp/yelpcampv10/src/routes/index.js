d11f15c3bd8083db56f8045c0cf230d8ded51561
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-05-12 14:36:01 -0400

    Cleanup.

--- web/udemy_web_bootcamp/backend/yelpcamp/yelpcampv10/src/routes/index.js
+++ web/udemy_web_bootcamp/backend/yelpcamp/yelpcampv10/src/routes/index.js
@@ -1,27 +1,27 @@
 // @flow
 // Express is our HTTP server.
 const express = require("express");
-// This is how we can export our routes into app.js
+// This is how we can export our routes into app.js.
 const router = express.Router();
 
 const User = require("../models/user");
 
-const passport = require("passport"); // Authentication API
+const passport = require("passport"); // Authentication API.
 
 // Add a GET request handler for /.
 router.get("/", function (exp_request, exp_response) {
-    console.log("GET @ /.");
-    exp_response.render("landing.ejs");
+  console.log(exp_request.method + " @ " + exp_request.originalUrl);
+  exp_response.render("landing.ejs");
 });
 
 // Auth. Routes.
 router.get("/register", function(exp_request, exp_response) {
-  console.log("GET @ /register.");
+  console.log(exp_request.method + " @ " + exp_request.originalUrl);
   exp_response.render("register.ejs");
 });
 
-router.post("/register", function(exp_request, exp_response){
-  console.log("POST @ /register.");
+router.post("/register", function(exp_request, exp_response) {
+  console.log(exp_request.method + " @ " + exp_request.originalUrl);
   console.log("exp_request.body = " + exp_request.body.username + "/" + exp_request.body.password);
   /*
     Register a new user. Passport does all the work behind the scenes to ensure
@@ -35,7 +35,7 @@ router.post("/register", function(exp_request, exp_response){
   User.register(
     new User({username: exp_request.body.username}),
     exp_request.body.password,
-    function(err, registeredUser){
+    function(err, registeredUser) {
       if(err) {
         console.log("User.register() error: ");
         console.log(err);
@@ -45,44 +45,53 @@ router.post("/register", function(exp_request, exp_response){
       // This is the "middleware" pattern ostensibly.
       console.log("User.register() successful: ");
 
-      /* "At some point passport.authenticate() calls User.serializeUser()."
-        FYI: "local" strategy can be swapped with e.g. "twitter". Secret page
-        code along pt.3. "This actually logs in the user" */
+      /*
+        "At some point passport.authenticate() calls User.serializeUser()".
+        FYI: "local" strategy can be swapped with e.g. "twitter". See Secret
+        page code along pt.3. "This actually logs in the user".
+      */
 
       /*
         FYI: exp_request and exp_response are the same as above, we are
         passing them as arguments to passport.authenticate("local")();
       */
-      passport.authenticate("local")(exp_request, exp_response, function(){
-         /* Successfully registered. Login the user and redirect
-         them to a page that is appropriate after registration. */
-         exp_response.redirect("/campgrounds");
+      passport.authenticate("local")(exp_request, exp_response, function() {
+        /*
+          Successfully registered. Login the user and redirect them to a page
+          that is appropriate after registration.
+        */
+        exp_response.redirect("/campgrounds");
       });
     });
 
 });
 
-router.get("/login", function(exp_request, exp_response){
-  console.log("GET @ /login.");
+router.get("/login", function(exp_request, exp_response) {
+  console.log(exp_request.method + " @ " + exp_request.originalUrl);
   exp_response.render("login.ejs"); // Show login form.
 });
 
-/* The call to passport.authenticate() here is called "middleware".
-Middleware = "code that runs  before our final route callback". Middleware can be
-"stacked up". */
+/*
+  The call to passport.authenticate() here is called "middleware". Middleware =
+  "code that runs  before our final route callback". Middleware can be "stacked
+  up".
+*/
 router.post("/login", // This route logs in a user and establishes a session.
   passport.authenticate("local", {successRedirect: "/campgrounds", failureRedirect: "/login"}),
-  function(exp_request, exp_response){
+  function(exp_request, exp_response) {
 
-  console.log("POST @ /login.");
+  console.log(exp_request.method + " @ " + exp_request.originalUrl);
   exp_response.send("Theres nothing like a nice Kraft Dinner after a hard day's work.");
 });
 
 router.get("/logout", function(exp_request, exp_response){
-  console.log("GET @ /logout.");
-  /* This method is from passport, it destroys the user's session. // Logs out
-  the user. // How does this function know which user to log out? ANS: The user
-  info is inside exp_request. */
+  console.log(exp_request.method + " @ " + exp_request.originalUrl);
+  /*
+    This method is from passport, it destroys the user's session.
+    Logs out the user.
+    How does this function know which user to log out? ANS: The user info is
+    inside exp_request.
+  */
   exp_request.logout();
   exp_response.redirect("/campgrounds");
 });
