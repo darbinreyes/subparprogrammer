cfd3aad23695bac4d02ff65dd535d471b09fe4d2
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-05-20 17:12:24 -0400

    Verified alphabetic sort works. Next: cleanup and add 2 more test cases.

--- coding_interviews/q_2/test_suite_one.c
+++ coding_interviews/q_2/test_suite_one.c
@@ -74,9 +74,53 @@ void test_0()
   }
 }
 
+void test_1() // This test case is the same as above except "tablet" is replaced with "atablet", I used it to verify the alphabetic sort works.
+{
+  int input_numToys, input_topToys, input_numQuotes;
+  char *input_toys_arr[] = {"elmo", "elsa", "legos", "drone", "atablet", "warcraft"};
+  char *input_quotes_arr[] = {
+  "Elmo is the hottest toy of the season! Elmo will be on every kid's wishlist!",
+  "The new Elmo dolls are super high quality.",
+  "Expect the Elsa dolls to be very popular this year",
+  "Elsa and Elmo are the toys I'll be buying for my kids",
+  "For parents of older kids, look into buying them a drone.",
+  "Warcraft is slowly rising in popularity ahead of the holiday season"
+  };
+  char *expected_result_arr[] = {"elmo", "elsa"};
+  int expected_result_size = 2;
+  boundedStringArray *computed_result, input_toys, input_quotes, expected_result;
+
+  input_numToys = 6;
+  input_topToys = 2;
+  input_numQuotes = 6;
+
+  input_toys.size = input_numToys;
+  input_toys.arr = input_toys_arr;
+
+  input_quotes.size = input_numQuotes;
+  input_quotes.arr = input_quotes_arr;
+
+  expected_result.size = expected_result_size;
+  expected_result.arr = expected_result_arr;
+
+// boundedStringArray* popularNToys(int numToys, int topToys,
+//                                     boundedStringArray* toys,
+//                                     int numQuotes,
+//                                     boundedStringArray* quotes);
+
+  computed_result = popularNToys(input_numToys, input_topToys, &input_toys, input_numQuotes, &input_quotes);
+
+  assert_int_equal(expected_result.size, computed_result->size);
+
+  for(int i = 0; i < expected_result.size; i++) {
+    assert_string_equal(expected_result.arr[i], computed_result->arr[i]);
+  }
+}
+
 void test_fixture_one( void )
 {
   test_fixture_start();               // starts a fixture
   run_test(test_0);
+  run_test(test_1);
   test_fixture_end();                 // ends a fixture
 }
