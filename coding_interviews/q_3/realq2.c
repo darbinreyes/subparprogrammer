6c26ce37045cf9c71bb65b156637fe42bca77482
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-05-21 15:55:19 -0400

    Start next question.

---
+++ coding_interviews/q_3/realq2.c
@@ -0,0 +1,34 @@
+// INCLUDE HEADER FILES NEEDED BY YOUR PROGRAM
+// SOME LIBRARY FUNCTIONALITY MAY BE RESTRICTED
+// DEFINE ANY FUNCTION NEEDED
+
+/*
+ *** Scratch work.
+
+ cut films into scenes = take a video file, and break it up in smaller video files, break up at points in the video called scenes.
+automate - breaking up - individual shots
+individual shots = short video, from a particular camera angle.
+break up _individual shots_ into _scenes_
+scenes = a _sequence of shots_
+given helper function - film.breakup_into_shots_and_letter_label().
+duplicate shots get the same letter label.
+
+todo - partition(sequence_of_labels) into scenes.
+in a scene, no shot labels appear in more than 1 scene. ??? // I don't understand this.
+
+Output scene.length for each scene.
+
+*** Output
+
+WTF is up with that struct def. its formated all wonKy!
+
+Most of the work here is figuring out what the problem statement means.
+
+*/
+
+// FUNCTION SIGNATURE BEGINS, THIS FUNCTION IS REQUIRED
+boundedarray* lengthEachScene(charboundedarray* inputList)
+{
+    // WRITE YOUR CODE HERE
+}
+// FUNCTION SIGNATURE ENDS
