a4e67940dfb29a9de221cff953bcce536d6b4ac2
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-05-29 14:51:59 -0400

    Leetcode problem progress.

--- coding_interviews/leetcode/p807/leetcode_807.c
+++ coding_interviews/leetcode/p807/leetcode_807.c
@@ -3,3 +3,39 @@
   https://leetcode.com/problems/max-increase-to-keep-city-skyline/
 
 */
+
+/*
+
+Example:
+Input: grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+Output: 35
+Explanation:
+The grid is:
+[ [3, 0, 8, 4],
+  [2, 4, 5, 7],
+  [9, 2, 6, 3],
+  [0, 3, 1, 0] ]
+
+The skyline viewed from top or bottom is: [9, 4, 8, 7]
+The skyline viewed from left or right is: [8, 7, 9, 3]
+
+The grid after increasing the height of buildings without affecting skylines is:
+
+gridNew = [ [8, 4, 8, 7],
+            [7, 4, 7, 7],
+            [9, 4, 8, 7],
+            [3, 3, 3, 3] ]
+
+******* Scratch Work
+
+"The skyline viewed from top or bottom is: [9, 4, 8, 7]"
+= Max value in each column of the original grid.
+
+"The skyline viewed from left or right is: [8, 7, 9, 3]"
+= Max value of each row of the original grid.
+
+*/
+
+int maxIncreaseKeepingSkyline(int** grid, int gridSize, int* gridColSize){
+// ? Is gridColSize really a row size?
+}
