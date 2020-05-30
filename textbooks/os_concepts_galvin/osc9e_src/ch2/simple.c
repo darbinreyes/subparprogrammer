da58d151fac8abdd1b4089adcdd193072de88dbb
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-05-30 03:56:14 -0400

    Add linux header files for reference. I'm not sure if man pages exist. Using this as docs for now.

--- textbooks/os_concepts_galvin/osc9e_src/ch2/simple.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch2/simple.c
@@ -2,17 +2,42 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#include <linux/list.h> // list
+#include <linux/types.h> // list_head
+#include <linux/slab.h> // kmalloc(), kfree()
+
+struct birthday {
+  char *name;
+  int year;
+  struct list_head list;
+};
+
+static LIST_HEAD(birthday_list);
+
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
-       printk(KERN_INFO "Loading Module\n");
+  struct birthday *tn;
+  char names_arr[] = {"Edsgar Dijkstra", "Tony Hoare", "Donald Knuth", "Derek Decker", "Euclid"};
+  int years_arr[] = {1965, 1977, 1979, 1981, 323};
+  printk(KERN_INFO "Loading Module\n");
 
-       return 0;
+  // Create linked list of 5 nodes.
+
+  for (int i = 0; i < 5; i++) {
+    tn = kmalloc(sizeof(*tn), GFP_KERNEL);
+    tn->name = names_arr[i];
+    tn->year = years_arr[i];
+    INIT_LIST_HEAD(&tn->list);
+    list_add_tail(&tn->list, birthday_list);
+  }
+
+  return 0;
 }
 
 /* This function is called when the module is removed. */
 void simple_exit(void) {
-	printk(KERN_INFO "Removing Module\n");
+  printk(KERN_INFO "Removing Module\n");
 }
 
 /* Macros for registering module entry and exit points. */
