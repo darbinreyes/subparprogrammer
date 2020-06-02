e77ad000ecdc52df29c22b78f935ea6d66fcf881
Darbin Emmanuel Reyes <darbin.e.reyes@gmail.com>
2020-06-02 13:22:19 -0400

    Problem progress.

--- coding_interviews/leetcode/p807/leetcode_807.c
+++ coding_interviews/leetcode/p807/leetcode_807.c
@@ -16,8 +16,8 @@ The grid is:
   [9, 2, 6, 3],
   [0, 3, 1, 0] ]
 
-The skyline viewed from top or bottom is: [9, 4, 8, 7]
-The skyline viewed from left or right is: [8, 7, 9, 3]
+The skyline viewed from top or bottom is: [9, 4, 8, 7] // col. 0 - col. (n - 1)
+The skyline viewed from left or right is: [8, 7, 9, 3] // row 0 - row (n - 1)
 
 The grid after increasing the height of buildings without affecting skylines is:
 
@@ -34,8 +34,52 @@ gridNew = [ [8, 4, 8, 7],
 "The skyline viewed from left or right is: [8, 7, 9, 3]"
 = Max value of each row of the original grid.
 
+Does the following work?
+
+For a given i, j , we may increment under the constraint that the result of
+incrementing is at most column max, and at most row max.
+
+From this it is manifest that we can increment grid 1, j min(col. max, row max).
+
+*******
+
+In the above example i, j = 0, 0
+
+row max = 8  = lr_max[i=0]
+col. max = 9 = tb_max[j=0]
+
+min(8, 9) = 8 = grid_max.
+
+inc = grid_max - grid[i][j]
+
+********
+
+i, j = 0, 1
+
+row max = 8 = lr_max[i=0]
+col. max = 4 = tb_max[j=1]
+
+min(8, 4) = 4 = grid_max
+
+inc = grid_max - grid[i][j]
+
+********
+
+i, j = 1, 2
+
+row max = 7 = lr_max[i=1]
+col. max = 8 = tb_max[j=2]
+
+min(7, 8) = 7 = grid_max
+
+
 */
 
+#include <assert.h>
+
+#define MAX(a,b) (  ( (a) > (b) ) ? (a) : (b) )
+#define MIN(a,b) (  ( (a) < (b) ) ? (a) : (b) )
+
 int maxIncreaseKeepingSkyline(int** grid, int gridSize, int* gridColSize){
-// ? Is gridColSize really a row size?
+  // ? Is gridColSize really a row size?
 }
