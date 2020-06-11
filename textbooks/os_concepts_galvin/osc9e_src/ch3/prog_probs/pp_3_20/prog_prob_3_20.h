aff62eb3d63f15f0bb8eb330779b344cdc0a127f
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-11 15:41:40 -0400

    Refactor.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_20/prog_prob_3_20.h
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_20/prog_prob_3_20.h
@@ -1,3 +1,12 @@
+#ifndef _PROG_PROB_3_20_H_
+#define _PROG_PROB_3_20_H_
+
+#define MIN_PID 300
+#define MAX_PID 5000
+#define NUM_PIDS (MAX_PID - MIN_PID + 1)
+
 int allocate_map(void);
 int allocate_pid(void);
 void release_pid(int pid);
+
+#endif
