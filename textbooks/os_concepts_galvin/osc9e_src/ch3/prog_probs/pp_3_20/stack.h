78ba9a0b908aba1ab165521d0d215a466db6e0dc
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-11 17:22:10 -0400

    Done. All test cases passed.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_20/stack.h
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_20/stack.h
@@ -6,4 +6,14 @@ int stack_pop(void);
 int stack_is_empty(void);
 void stack_init(void);
 
+/*
+
+  Test related functions.
+
+*/
+
+int const * const test_get_free_pid_stack(void);
+
+int test_get_stack_next_free(void);
+
 #endif
