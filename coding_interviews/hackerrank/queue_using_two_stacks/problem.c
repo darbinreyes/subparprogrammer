3ed95f33fb23b142255f3738dc3b1821e7d741da
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-16 12:50:27 -0400

    Problem progress.

--- coding_interviews/hackerrank/queue_using_two_stacks/problem.c
+++ coding_interviews/hackerrank/queue_using_two_stacks/problem.c
@@ -3,3 +3,26 @@
   https://www.hackerrank.com/challenges/queue-using-two-stacks/problem
 
 */
+
+
+/*
+******* Constraints
+
+1 <= q <= 100000.  // Q_SIZE = 100000.
+
+1 <= type <= 3. // Use #defines.
+
+1 <= abs(x) <= 1000000000. // <= 2**31-1. int is sufficient.
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  return 0;
+}
