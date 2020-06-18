a1c4899082929e88b11fcdd272fa31e347c7eb82
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-18 19:32:53 -0400

    Update port number. Add haiku string.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_24/DateClient.java
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_24/DateClient.java
@@ -5,7 +5,7 @@ public class DateClient
 {
   public static void main(String[] args) {
     try {
-      Socket sock = new Socket("127.0.0.1", 6013);
+      Socket sock = new Socket("127.0.0.1", 5575);
 
       InputStream in = sock.getInputStream();
       BufferedReader bin = new BufferedReader(new InputStreamReader(in));
--- textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_24/DateServer.java
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_24/DateServer.java
@@ -4,8 +4,11 @@ import java.io.*;
 public class DateServer
 {
   public static void main(String[] args) {
+    String haiku = "fu-ru-i-ke ya\n"
+                   + "ka-wa-zu to-bi-ko-mu\n"
+                   + "mi-zu-no-o-to";
     try {
-      ServerSocket sock = new ServerSocket(6013);
+      ServerSocket sock = new ServerSocket(5575);
 
       /* now listen for connections */
       while (true) {
