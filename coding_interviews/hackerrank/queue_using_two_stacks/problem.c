a70c978ff015d1c6bbccc2bc13e0fbb06528edf8
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-19 14:46:03 -0400

    Problem progress.

--- coding_interviews/hackerrank/queue_using_two_stacks/problem.c
+++ coding_interviews/hackerrank/queue_using_two_stacks/problem.c
@@ -21,6 +21,33 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define Q_SIZE (100000+1)
+#define IS_EN_Q 1
+#define IS_DE_Q 2
+#define IS_PRINT_Q_FRONT 3
+
+static int queue[Q_SIZE];
+static int front_index = 0;
+static int back_index = 0;
+
+// Returns 1 if the queue is full. Returns 0 otherwise.
+int is_full(void) {
+  if ( ((back_index + 1) % Q_SIZE) == front_index ) {
+    return 1;
+  }
+
+  return 0;
+}
+
+// Returns 1 if the queue is empty. Returns 0 otherwise.
+int is_empty(void) {
+  if(back_index == front_index) {
+    return 1;
+  }
+
+  return 0;
+}
+
 /*
   Add an entry to the back of the queue.
 */
