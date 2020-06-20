dc1ab3063298f780bdfcec7930db7e3b00e23e5f
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-19 20:35:32 -0400

    Make server read bytes instead of characters. Test that the read() loop terminates when the client closes the connection.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_25/EchoServer.java
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_25/EchoServer.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import java.lang.Integer;
 
 public class EchoServer
 {
@@ -10,13 +11,16 @@ public class EchoServer
       /* now listen for connections */
       while (true) {
         Socket client = sock.accept();
-
+        System.out.println("Client connected.");
         /* Read what the client sent us */
         InputStream in = client.getInputStream();
-        BufferedReader bin = new BufferedReader(new InputStreamReader(in));
-        String line;
-        while ( (line = bin.readLine()) != null)
-          System.out.println(line);
+        //BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+        //String line;
+        int next_byte;
+        while ( (next_byte = in.read()) != -1)
+          System.out.println(new Integer(next_byte));
+
+        System.out.println("Client closed connection.");
         /*  ******* */
         PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
 
