407f0fdb6e0b5609a2169a3b2a3f45dfd9a6e6ec
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-19 23:18:05 -0400

    Put some date on the heap.

--- c_projects/project0/p2/p2.c
+++ c_projects/project0/p2/p2.c
@@ -1,10 +1,19 @@
+#include <stdlib.h>
+
 char *str = "Hello Edsgar Dijkstra.";
 
 int main(int argc, char **argv) {
-
+  int *heap_data;
   int a = 5, b = 7, s = 0;
 
   s = a + b;
+
+  heap_data = malloc(sizeof(int) * 4); // Space for 4 integers.
+
+  heap_data[0] = 2; // Fill with primes.
+  heap_data[1] = 3;
+  heap_data[2] = 5;
+  heap_data[3] = 7;
 
   return 0;
 }
