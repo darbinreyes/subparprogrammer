c3de68a6235e8d0a4e562c5e4bac8c015ce67e7c
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-23 16:34:22 -0400

    Test sending non-character data. Cleanup prints.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_25/EchoClient.java
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_25/EchoClient.java
@@ -8,15 +8,18 @@ public class EchoClient
       Socket sock = new Socket("127.0.0.1", 6013);
 
       /* Send something to the server */
-      String send_str = "Hello, my name is Dijkstra.";
+      String send_str = "Hello Dijkstra.";
+      int[] primes = {2, 3, 5, 7, 11, 13};
       OutputStream out = sock.getOutputStream();
       InputStream in = sock.getInputStream();
-      int next_byte;
+      int send_byte;
+      int rec_byte;
 
-      for (int i = 0; i < send_str.length(); i++){
-        out.write(send_str.charAt(i)); // Send a byte
-        next_byte = in.read(); // Receive a byte
-        System.out.println(new Integer(next_byte)); // Print received byte.
+      for (int i = 0; i < primes.length/*send_str.length()*/; i++){
+        send_byte = primes[i]; // send_str.charAt(i);
+        out.write(send_byte); // Send a byte
+        rec_byte = in.read(); // Receive a byte
+        System.out.println("Sent/Received byte: " + new Integer(send_byte) + "/" + new Integer(rec_byte)); // Print received byte.
       }
 
       sock.close();
