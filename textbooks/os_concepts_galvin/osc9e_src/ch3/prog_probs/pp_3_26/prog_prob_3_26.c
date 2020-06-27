ad411ef8698a844ac248db60cba00c6b9b5f3536
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-26 22:38:32 -0400

    Code completed.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_26/prog_prob_3_26.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_26/prog_prob_3_26.c
@@ -2,10 +2,13 @@
 #include <stdio.h> // fprintf(), printf()
 #include <string.h> // strlen()
 #include <unistd.h> // pipe(), close(), write()
+#include <ctype.h> // toupper() etc.
 
 #define BUFFER_SIZE 25
 #define READ_END 0
 #define WRITE_END 1
+
+void toggle_str_case(char *s);
 
 int main(void) {
   char write_msg[BUFFER_SIZE] = "Edsger Dijkstra";
@@ -37,6 +40,7 @@ int main(void) {
 
   if (pid > 0) { /* parent process */
     /* Parent write */
+
     /* close the unused end of the pipe */
     close(p2c_fd[READ_END]);
 
@@ -46,16 +50,19 @@ int main(void) {
     /* close the write end of the pipe */
     close(p2c_fd[WRITE_END]);
     /* Parent read */
+
     /* close the unused end of the pipe */
     close(c2p_fd[WRITE_END]);
 
     /* read from the pipe */
     read(c2p_fd[READ_END], read_msg, BUFFER_SIZE);
     printf("Parent read. %s\n", read_msg);
+
     /* close the read end of the pipe */
     close(c2p_fd[READ_END]);
   } else { /* child process */
     /* Child read */
+
     /* close the unused end of the pipe */
     close(p2c_fd[WRITE_END]);
 
@@ -65,18 +72,35 @@ int main(void) {
 
     /* close the read end of the pipe */
     close(p2c_fd[READ_END]);
+
     /* Child write */
+
     /* close the unused end of the pipe */
     close(c2p_fd[READ_END]);
 
-    // TODO: Reverse casing of string.
-    // For now, just change the first character
-    read_msg[0] = 'X';
+    /* reverse casing of string */
+    toggle_str_case(read_msg);
+
     /* write to the pipe */
     write(c2p_fd[WRITE_END], read_msg, strlen(read_msg) + 1);
+
     /* close the write end of the pipe */
     close(c2p_fd[WRITE_END]);
   }
 
   return 0;
+}
+
+void toggle_str_case(char *s) {
+  char c;
+
+  while ( (c = *s) != '\0') {
+    if(isupper(c)) {
+      *s = tolower(c);
+    } else if(islower(c)) {
+      *s = toupper(c);
+    }
+
+    s++;
+  }
 }
