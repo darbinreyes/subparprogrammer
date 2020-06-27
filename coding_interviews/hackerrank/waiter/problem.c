3dbdcd9169d0ea32c39e4962f7bff4694bff6a33
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-26 20:45:21 -0400

    Add sample input/output and given code.

--- coding_interviews/hackerrank/waiter/problem.c
+++ coding_interviews/hackerrank/waiter/problem.c
@@ -5,3 +5,136 @@
   Success Rate: %82.79
 
 */
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+/*
+ * Complete the waiter function below.
+ */
+
+/*
+ * Please store the size of the integer array to be returned in result_count pointer. For example,
+ * int a[3] = {1, 2, 3};
+ *
+ * *result_count = 3;
+ *
+ * return a;
+ *
+ */
+int* waiter(int number_count, int* number, int q, int* result_count) {
+    /*
+     * Write your code here.
+     */
+
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** nq = split_string(readline());
+
+    char* n_endptr;
+    char* n_str = nq[0];
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* q_endptr;
+    char* q_str = nq[1];
+    int q = strtol(q_str, &q_endptr, 10);
+
+    if (q_endptr == q_str || *q_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char** number_temp = split_string(readline());
+
+    int number[n];
+
+    for (int number_itr = 0; number_itr < n; number_itr++) {
+        char* number_item_endptr;
+        char* number_item_str = number_temp[number_itr];
+        int number_item = strtol(number_item_str, &number_item_endptr, 10);
+
+        if (number_item_endptr == number_item_str || *number_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        number[number_itr] = number_item;
+    }
+
+    int result_count;
+    int* result = waiter(number_count, number, q, &result_count);
+
+    for (int result_itr = 0; result_itr < result_count; result_itr++) {
+        fprintf(fptr, "%d", result[result_itr]);
+
+        if (result_itr != result_count - 1) {
+            fprintf(fptr, "\n");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
