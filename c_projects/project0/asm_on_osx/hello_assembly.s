536d807cc7a0e6dbac3c5f3c8149ada5ff3a80e1
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-27 22:45:03 -0400

    Assembly code I wrote as part of reading "Writing 64 Bit Assembly on Mac OS X".
    http://www.idryman.org/blog/2014/12/02/writing-64-bit-assembly-on-mac-os-x/

---
+++ c_projects/project0/asm_on_osx/hello_assembly.s
@@ -0,0 +1,22 @@
+# Simple assembly program.
+# Main function that calls the exit system call
+.section __TEXT,__text
+.globl _main
+
+_main:
+
+  movl $0x2000001, %eax # system call $1 with $0x2000000 offset. See comments at bottom of this file.
+  movl $7, %ebx # set the exit code to be $7.
+  syscall # issue a system call.
+
+# System call numbers can be "found in /usr/include/sys/syscall.h".
+# On my machine that file is elsewhere, it occurs in many places.
+# Here's one: "/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/sys/syscall.h"
+## define SYS_syscall        0
+## define SYS_exit           1
+## define SYS_fork           2
+## define SYS_read           3
+## define SYS_write          4
+## define SYS_open           5
+## define SYS_close          6
+## define SYS_wait4          7
