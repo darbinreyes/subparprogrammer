3de0e69db36bd28efc699b1ec1ca2ef462eef9a8
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-29 15:45:23 -0400

    Done with this part.

--- c_projects/project0/asm_on_osx/part3/hello_assembly.s
+++ c_projects/project0/asm_on_osx/part3/hello_assembly.s
@@ -1,14 +1,24 @@
 # Simple assembly program.
 # Main function that calls the write system call, i.e.:
 #  ssize_t write(int fildes, const void *buf, size_t nbyte);
+
 .section __TEXT,__text
 .globl _main
-
 _main:
-
+  pushq %rbp
+  movq %rsp, %rbp
   movl $0x2000004, %eax # system call $4 with $0x2000000 offset. See comments at bottom of this file.
-  movl $7, %edi # set the exit code to be $7.
+  movl $1, %edi # set 1st argument to stdout fd.
+  movq str@GOTPCREL(%rip), %rsi # set 2nd argument to string pointer. GOT = (Global Offset Table). https://developer.apple.com/library/archive/documentation/DeveloperTools/Conceptual/MachOTopics/1-Articles/x86_64_code.html
+  movq $23, %rdx
   syscall # issue a system call.
+  movl $7, %eax # Return 7 from exit.
+  popq %rbp
+  retq
+
+.section __DATA,__data
+str:
+  .asciz "Hello Edsger Dijkstra.\n"
 
 # System call numbers can be "found in /usr/include/sys/syscall.h".
 # On my machine that file is elsewhere, it occurs in many places.
