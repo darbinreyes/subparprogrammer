a922ebee2fa5864b339db3e5817795a32b8f96f2
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-06-29 14:15:17 -0400

    Second part of tutorial.
    http://www.idryman.org/blog/2014/12/02/writing-64-bit-assembly-on-mac-os-x/

---
+++ c_projects/project0/asm_on_osx/part1/hello_assembly.s
@@ -0,0 +1,24 @@
+# Simple assembly program.
+# Main function that calls the exit system call using callq instruction instead of syscall instruction.
+.section __TEXT,__text
+.globl _main
+
+_main:
+
+  pushq %rbp # System V calling convention for x64
+  movq %rsp, %rbp
+  movl $0x2000001, %eax # system call $1 with $0x2000000 offset. See comments at bottom of this file.
+  movl $7, %edi # set the exit code to be $7.
+  syscall # issue a system call.
+
+# System call numbers can be "found in /usr/include/sys/syscall.h".
+# On my machine that file is elsewhere, it occurs in many places.
+# Here's one: "/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/usr/include/sys/syscall.h"
+## define SYS_syscall        0
+## define SYS_exit           1
+## define SYS_fork           2
+## define SYS_read           3
+## define SYS_write          4
+## define SYS_open           5
+## define SYS_close          6
+## define SYS_wait4          7
