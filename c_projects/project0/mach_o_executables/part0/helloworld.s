1cc938fca4ad49f7e1793566356bff6eb3d9bfb7
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-07-01 09:08:29 -0400

    Add command used.

--- c_projects/project0/mach_o_executables/part0/helloworld.s
+++ c_projects/project0/mach_o_executables/part0/helloworld.s
@@ -1,3 +1,4 @@
+	# Output of xcrun clang -S -o helloworld.s  helloworld.c
 	.section	__TEXT,__text,regular,pure_instructions
 	.build_version macos, 10, 15	sdk_version 10, 15, 4
 	.globl	_main                   ## -- Begin function main
