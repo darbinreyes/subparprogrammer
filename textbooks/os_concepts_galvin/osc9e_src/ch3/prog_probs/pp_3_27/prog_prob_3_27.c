80f9c03729a6a85c404b6197aff76af869d36202
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-07-03 16:38:38 -0400

    Add loop in child process to read from the pipe in buffer sized chunks. Handle all errors w.r.t. reading to the pipe and writing to the destination file.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_27/prog_prob_3_27.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/prog_probs/pp_3_27/prog_prob_3_27.c
@@ -41,117 +41,83 @@ int main(int argc, char **argv) {
     return 3;
   }
 
-  if (pid > 0) { /* parent process */
 
-    /* Read from the source file. */
+
+  if (pid > 0) { /* parent process */
+    /* Open the source file */
     src = fopen(src_name, "r");
 
     if (src == NULL) {
       fprintf(stderr, "fopen failed for file name %s.\n", src_name);
-      return 4;
+      return -1;
     }
 
-    /* man fread
-
-      If an error occurs, or the end-of-file is reached, the return value is a
-      short object count (or zero).
-
-      The function fread() does not distinguish between end-of-file and error;
-      callers must use feof(3) and ferror(3) to determine which occurred.
-
-    */
+    /* Parent. close the unused end of the pipe */
+    close(fd[READ_END]);
 
     while(!feof(src)) {
+      /* man fread
+        If an error occurs, or the end-of-file is reached, the return value is a short object count (or zero).
+        The function fread() does not distinguish between end-of-file and error; callers must use feof(3) and ferror(3) to determine which occurred.
+      */
+      /* Read from the source file. */
       n = fread(cpbuf, sizeof(char), BUFFER_SIZE, src);
-      if (n < BUFFER_SIZE && ferror(src)) {
+
+      if (n < BUFFER_SIZE && ferror(src)) { // Bail if an error occurs.
         fprintf(stderr, "An error occurred for file name %s.\n", src_name);
         perror(src_name);
         return -1;
       }
+
+      /* Parent. write to the pipe */
+      nw = write(fd[WRITE_END], cpbuf, n); // Note: The textbook doesn't check for errors here. I leave it that way.
+      printf("Parent wrote %d bytes.\n", nw);
     }
 
-    if (n <= BUFFER_SIZE) { // Either we reached EOF or an error occurred.
-      if (!feof(src)) {
-        // We didn't reach EOF.
-        if (ferror(src)) {
-          // File related error occurred.
-          fprintf(stderr, "An error occurred for file name %s.\n", src_name);
-          perror(src_name);
-          return 5;
-        } else if (n < BUFFER_SIZE) {
-          fprintf(stderr, "Unexpected. fread returned short object count but ferror returned 0. file name %s.\n", src_name);
-          return 6;
-        } if (n == BUFFER_SIZE) {
-          // FYI: We don't reach EOF if we fread exactly as many bytes as the file contains.
-          fprintf(stderr, "file name %s. File copy will be truncated if its size is greater than %d bytes.\n", src_name, BUFFER_SIZE);
-        }
-      }
-    } else {
-      fprintf(stderr, "fread returned more bytes than size of buffer. This should never happen. file name %s.\n", src_name);
-      return 7;
-    }
-
-    if (fclose(src)) {
-      // Failed to close the file, but we already the its contents so we can still continue.
-      // Inform user.
+    /* Failed to close the file, but we already the its contents so we can still continue. Inform user. */
+    if (fclose(src))
       fprintf(stderr, "fclose failed for file name %s.\n", src_name);
-    }
-
-    /* Parent write to pipe. */
-    /* close the unused end of the pipe */
-    close(fd[READ_END]);
-
-    /* write to the pipe */
-    nw = write(fd[WRITE_END], cpbuf, n); // Note: The textbook doesn't check for errors here. I leave it that way.
-    printf("Parent wrote %d bytes.\n", nw);
 
     /* close the write end of the pipe */
     close(fd[WRITE_END]);
   } else { /* child process */
-
+    /* Open the destination file */
+    dest = fopen(dest_name, "w");
+    if (dest == NULL) {
+      fprintf(stderr, "fopen failed for file name %s.\n", dest_name);
+      return -1;
+    }
     /* Child read from pipe */
     /* close the unused end of the pipe */
     close(fd[WRITE_END]);
 
     /* read from the pipe */
-    n = read(fd[READ_END], cpbuf, BUFFER_SIZE);
-
-    printf("Child read %d bytes.\n", n);
+    while ( (n = read(fd[READ_END], cpbuf, BUFFER_SIZE)) != 0 ) {
+      if(n == -1) {
+        // Bail if reading from the pipe fails.
+        fprintf(stderr, "Child. An error occurred reading from the pipe.\n");
+        perror("Child");
+        return -1;
+      }
+      printf("Child read %d bytes.\n", n);
+      /* man fwrite
+        If an error occurs, or the end-of-file is reached, the return value is a short object count (or zero).
+        The function fwrite() returns a value less than nitems only if a write error has occurred.
+      */
+      /* Write to the destination file. */
+      nw = fwrite(cpbuf, sizeof(char), n, dest);
+      if (nw < n) {
+        fprintf(stderr, "Error writing to file name %s.\n", dest_name);
+        perror("Child");
+        return -1;
+      }
+    }
 
     /* close the read end of the pipe */
     close(fd[READ_END]);
 
-    /* Write to the destination file. */
-    dest = fopen(dest_name, "w");
-    if (dest == NULL) {
-      fprintf(stderr, "fopen failed for file name %s.\n", dest_name);
-      return 4;
-    }
-
-    nw = fwrite(cpbuf, sizeof(char), n, dest);
-    /* man fwrite
-
-      If an error occurs, or the end-of-file is reached, the return value is a
-      short object count (or zero).
-
-      The function fwrite() returns a value less than nitems only
-      if a write error has occurred.
-
-    */
-    if (nw < n) {
-      fprintf(stderr, "fwrite error for file name %s.\n", dest_name);
-      if (ferror(dest)) {
-        // File related error occurred.
-        fprintf(stderr, "An error occurred for file name %s.\n", dest_name);
-        perror(dest_name);
-        //return 8;
-      }
-    }
-
-    if (fclose(dest)) {
+    if (fclose(dest))
       fprintf(stderr, "fclose failed for file name %s.\n", dest_name);
-      return 8;
-    }
   }
 
   return 0;
