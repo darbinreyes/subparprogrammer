fad995668d16e963a44be6d8133f6e55f8c82f9c
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-07-10 18:48:34 -0400

    Implement basic input loop.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/programming_projects/project_1_part_1/project_1_part_1.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/programming_projects/project_1_part_1/project_1_part_1.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+
+#define LINE_BUFFER_SIZE 1024
 
 int main(int argc, char **argv) {
+    int should_run = 1;
+    char line[LINE_BUFFER_SIZE];
+    char *l;
+
+    while (should_run) {
+        printf("darbinsshell> ");
+
+        /* Get a line of input */
+        line[0] = '\0';
+        l = fgets(line, LINE_BUFFER_SIZE, stdin);
+
+        /* Check if fgets encountered an error */
+        if (l == NULL && ferror(stdin)) {
+            fprintf(stderr, "fgets error. Bye.\n");
+            return 1;
+        }
+
+        if (l == NULL && feof(stdin)) {
+            fprintf(stderr, "fgets got eof. Bye.\n");
+            return 3;
+        }
+
+        /* Check if the input line was too long */
+        if (strlen(line) == LINE_BUFFER_SIZE - 1 && line[LINE_BUFFER_SIZE - 2] != '\n') {
+            fprintf(stderr, "Input line was too long.\n");
+            return 2;
+        }
+
+        printf("You entered: %s", line);
+
+        /* Check is the user wants to exit */
+        if(strcmp(line, "exit\n") == 0) {
+            printf("Ok. Goodbye!\n");
+            should_run = 0;
+        }
+    }
 
     return 0;
 }
