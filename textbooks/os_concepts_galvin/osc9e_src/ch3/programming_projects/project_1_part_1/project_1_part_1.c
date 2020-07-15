4d11e7b76d30489c66e791f35a16b9c2fe5284c4
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-07-15 11:44:42 -0400

    Use 80 character line size per problem description. Implement input parsing into space delimited tokens. Handle case of too many input tokens.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/programming_projects/project_1_part_1/project_1_part_1.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/programming_projects/project_1_part_1/project_1_part_1.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-#define LINE_BUFFER_SIZE 1024
+#define LINE_BUFFER_SIZE 80
+
+// Returns 0 if successful. At most max - 1 tokens are returned. Returns 1 if there were more than max - 1 tokens in line.
+// Parses line into space separated tokens, returns tokens in args.
+// The last token in args is indicated by a subsequent NULL pointer.
+int parse_input(char *line, char **args, int max) {
+    int i = 0;
+    char *s;
+    s = strtok(line, " \n");
+
+    while (s != NULL && i < max - 1) {
+        args[i] = s;
+        i++;
+        s = strtok(NULL, " \n");
+    }
+
+    args[i] = NULL;
+
+    if (s != NULL && i == max - 1) {
+        return 1;
+    }
+
+    return 0;
+}
 
 int main(int argc, char **argv) {
+    char *args[4];//LINE_BUFFER_SIZE/2+1];
     int should_run = 1;
     char line[LINE_BUFFER_SIZE];
     char *l;
 
     while (should_run) {
         printf("darbinsshell> ");
+        // ? fflush(stdout)
 
         /* Get a line of input */
         line[0] = '\0';
@@ -33,6 +58,10 @@ int main(int argc, char **argv) {
         }
 
         printf("You entered: %s", line);
+        if(parse_input(line, args, sizeof(args)/sizeof(args[0]))) {
+            fprintf(stderr, "Input line had too many tokens.\n");
+            return 1;
+        }
 
         /* Check is the user wants to exit */
         if(strcmp(line, "exit\n") == 0) {
