163054331f0ed8e48b1aa7184a41462a604466a6
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-07-15 14:13:44 -0400

    First draft implementation complete.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/programming_projects/project_1_part_1/project_1_part_1.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/programming_projects/project_1_part_1/project_1_part_1.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define LINE_BUFFER_SIZE 80
 
 
-int parse_input(char *line, char **args, int max);
-
+int parse_input(char *line, char **args, int max, int *no_wait);
+pid_t run_cmd(char **args, int no_wait);
 
 int main(int argc, char **argv) {
     char *args[LINE_BUFFER_SIZE/2+1];
     int should_run = 1;
     char line[LINE_BUFFER_SIZE];
     char *l;
+    int no_wait;
+    pid_t pid;
 
     while (should_run) {
         printf("darbinsshell> ");
-        // ? fflush(stdout) // The use of this function might be evident after adding fork()+exec() call.
+        //fflush(stdout); // The use of this function might be evident after adding fork()+exec() call.
 
         /* Get a line of input */
         line[0] = '\0';
@@ -39,16 +43,24 @@ int main(int argc, char **argv) {
             return 2;
         }
 
-        printf("You entered: %s", line);
-        if(parse_input(line, args, sizeof(args)/sizeof(args[0]))) {
+        //printf("You entered: %s", line);
+        if (parse_input(line, args, sizeof(args)/sizeof(args[0]), &no_wait)) {
             fprintf(stderr, "Input line had too many tokens.\n");
             return 1;
         }
 
         /* Check is the user wants to exit */
-        if(args[0] != NULL && strcmp(args[0], "exit") == 0) {
+        if (args[0] != NULL && strcmp(args[0], "exit") == 0) {
             printf("Ok. Goodbye!\n");
             should_run = 0;
+        }
+
+        if (args[0] != NULL) {
+            pid = run_cmd(args, no_wait);
+            if (pid <= 0) {
+                fprintf(stderr, "Command error. Bye.\n");
+                return 1;
+            }
         }
     }
 
@@ -58,9 +70,12 @@ int main(int argc, char **argv) {
 // Returns 0 if successful. At most max - 1 tokens are returned. Returns 1 if there were more than max - 1 tokens in line.
 // Parses line into space separated tokens, returns tokens in args.
 // The last token in args is indicated by a subsequent NULL pointer.
-int parse_input(char *line, char **args, int max) {
+int parse_input(char *line, char **args, int max, int *no_wait) {
     int i = 0;
     char *s;
+
+    *no_wait = 0;
+
     s = strtok(line, " \n");
 
     while (s != NULL && i < max - 1) {
@@ -75,5 +90,45 @@ int parse_input(char *line, char **args, int max) {
         return 1;
     }
 
+
+    if (i > 0 && args[i - 1][0] == '&') {
+        *no_wait = 1;
+        args[i - 1] = NULL; // This prevents the '&' from being interpreted as an argument.
+    }
+
     return 0;
+}
+
+/*
+
+    Creates a child process using fork(). The child calls execvp() and the
+    parent waits for the child to terminate before returning. If no_wait != 0
+    the parent does not wait and instead returns immediately.
+
+*/
+pid_t run_cmd(char **args, int no_wait) {
+    pid_t pid;
+    int stat_loc;
+
+    pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "fork() error.\n");
+    } else if (pid > 0) {
+        /* Parent process */
+        if(no_wait)
+            ;//fprintf(stderr, "Parent not waiting.\n");
+        else {
+            wait(&stat_loc);
+            fprintf(stderr, "Parent returned from wait.\n");
+        }
+
+    } else {
+        /* Child process */
+        execvp(args[0], args); // Only returns upon error.
+        fprintf(stderr, "\nexec() error.\n");
+        exit(1);
+    }
+
+    return pid;
 }
