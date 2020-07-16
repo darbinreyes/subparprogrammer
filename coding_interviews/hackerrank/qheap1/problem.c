0f91591e8421749dab85bde44a30f2a38bc08086
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-07-16 11:04:30 -0400

    Reviewing heap implementation.

--- coding_interviews/hackerrank/qheap1/problem.c
+++ coding_interviews/hackerrank/qheap1/problem.c
@@ -5,3 +5,23 @@
     Success Rate: 67.73%
 
 */
+
+/*
+
+    Notes from Carrano, Chapter 26, A Heap Implementation.
+
+    A heap is a complete binary tree whose nodes are ordered in specific manner.
+    In a maxheap each node is greater than or equal to the objects in the nodes descendants.
+
+    The interface to a maxheap has the following methods:
+    void add(T newEntry)
+    T removeMax();
+    T getMax();
+    isEmpty();
+    getSize();
+    clear();
+
+    NEXT: pg. 707, "Since the tree is complete."
+
+
+*/
