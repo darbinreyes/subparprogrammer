618d275cf0eb69a2e125584e5568099b9c2f169f
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-07-29 15:19:38 -0400

    First draft implementation.

--- textbooks/os_concepts_galvin/osc9e_src/ch3/programming_projects/project_2_part_2/simple.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch3/programming_projects/project_2_part_2/simple.c
@@ -5,18 +5,23 @@
 /* ******* My includes ******* */
 #include <linux/sched.h> // `for_each_process()`, `struct task_struct`.
 
-
+void dfs_task_iter(struct task_struct *t) {
+    struct list_head *list;
+    struct task_struct *c;
+    list_for_each(list, &t->children) {
+        c = list_entry(list, struct task_struct, children);
+        printk(KERN_INFO "%ld %d %s\n", c->state, c->pid, c->comm);
+        dfs_task_iter(c);
+    }
+}
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
-    struct task_struct *t = NULL;
+    struct task_struct *t = &init_task;
     printk(KERN_INFO "Dijkstra Loading Module\n");
 
-    printk(KERN_INFO "S PID CMD\n");
-    for_each_process(t) {
-        printk(KERN_INFO "%ld %d %s\n", t->state, t->pid, t->comm);
-    }
-
+    printk(KERN_INFO "%ld %d %s\n", t->state, t->pid, t->comm);
+    dfs_task_iter(t);
     return 0;
 }
 
