3c1baf14af534259d28bec9d30dab1d14c2dd656
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-08-07 14:49:33 -0400

    Fix compilation error in GIVEN code. WTF.

---
+++ coding_interviews/hackerrank/jesse-and-cookies/given.c
@@ -0,0 +1,113 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+/*
+ * Complete the cookies function below.
+ */
+int cookies(int k, int A_count, int* A) {
+    /*
+    * Write your code here.
+    */
+    return 0;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** nk = split_string(readline());
+
+    char* n_endptr;
+    char* n_str = nk[0];
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char* k_endptr;
+    char* k_str = nk[1];
+    int k = strtol(k_str, &k_endptr, 10);
+
+    if (k_endptr == k_str || *k_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char** A_temp = split_string(readline());
+
+    int A[n];
+
+    for (int A_itr = 0; A_itr < n; A_itr++) {
+        char* A_item_endptr;
+        char* A_item_str = A_temp[A_itr];
+        int A_item = strtol(A_item_str, &A_item_endptr, 10);
+
+        if (A_item_endptr == A_item_str || *A_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        A[A_itr] = A_item;
+    }
+
+    int result = cookies(k, n, A);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
