e4d23b7f0c99e4ae53af7ba6065355f08b66ad02
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-08-10 21:31:07 -0400

    Start next problem.

---
+++ coding_interviews/hackerrank/down-to-zero-ii/problem.c
@@ -0,0 +1,15 @@
+/*
+
+    https://www.hackerrank.com/challenges/down-to-zero-ii/problem
+
+    Success Rate: 43.63%
+
+*/
+
+/*
+
+    If N is prime, our only choice is to reduce N by 1.
+    Otherwise it is always better to reduce N by more than 1, i.e. reduce N down
+    to greatest_measure.(N)
+
+*/
