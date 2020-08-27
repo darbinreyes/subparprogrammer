d3dadfb381327dda7f9a1281e82f881302a81209
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-08-27 18:18:05 -0400

    Single threaded solution.

--- textbooks/os_concepts_galvin/osc9e_src/ch4/programming_problems/pp_4_21/prog_prob_4_21.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch4/programming_problems/pp_4_21/prog_prob_4_21.c
@@ -4,33 +4,85 @@
 #define ARRAY_SIZE 10
 
 long a[ARRAY_SIZE];
+int as;
+
+long avg, min, max;
+
+void *avg_thread(void *param){
+
+    avg = 0;
+
+    for(int i = 0; i < as; i++) {
+        avg += a[i]; // FYI: Not checking for overflow.
+    }
+
+    avg /= as;
+
+    return NULL;
+}
+
+void *min_thread(void *param){
+    long t;
+
+    t = a[0];
+    for(int i = 1; i < as; i++) {
+        if (a[i] < t)
+            t = a[i];
+    }
+
+    min = t;
+
+    return NULL;
+}
+
+void *max_thread(void *param){
+
+    long t;
+
+    t = a[0];
+    for(int i = 1; i < as; i++) {
+        if (a[i] > t)
+            t = a[i];
+    }
+
+    max = t;
+
+    return NULL;
+}
 
 int main(int argc, char **argv) {
 
     int i;
-    long *ap;
 
     if(argc <= 1) {
-        printf("Usage: Provide up to 100 positive integers. \
-            More than that will be ignored. Example./a.out 2 3 5 7.\n");
+        printf("Usage: Provide up to %d positive integers. "
+               "More than that will be ignored. Example ./a.out 2 3 5 7.\n", ARRAY_SIZE);
         return 1;
     }
 
-    // for (i = 1; i < argc && i < ARRAY_SIZE+1; i++) {
-    //     a[i-1] = strtol
-    // }
-
-    ap = &a;
-    argv++;
-    argc--;
-
-    while(argc-- > 0) {
-        *ap = strtol(*argv, NULL, 10);
-        if (*ap == 0) {
-            printf("Failed to convert %s to an integer. Aborted.\n", *argv);
+    for (i = 0; i < ARRAY_SIZE && i < argc - 1; i++) {
+        a[i] = strtol(argv[i+1], NULL, 10);
+        if (a[i] == 0) {
+            printf("Failed to convert \"%s\" to an integer. Aborted.\n", argv[i+1]);
             return -1;
         }
     }
+
+    as = i;
+
+    printf("%d inputs: ", as);
+
+    for(i = 0; i < as; i++) {
+        printf("%ld ", a[i]);
+    }
+
+    printf("\n");
+
+    avg_thread(NULL);
+    min_thread(NULL);
+    max_thread(NULL);
+
+    printf("avg. %ld\nmin. %ld\nmax. %ld\n", avg, min, max);
 
     return 0;
 }
