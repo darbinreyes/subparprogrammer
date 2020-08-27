2bba2e3a85e5f5ae37f8ec93a7cfbcc37d167664
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-08-27 10:03:45 -0400

    Given code.

---
+++ coding_interviews/hackerrank/simple-text-editor/problem.c
@@ -0,0 +1,16 @@
+/*
+
+    https://www.hackerrank.com/challenges/simple-text-editor/problem
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
