1e36cf3fe72e1a19aed67a01cb0f9556938fad95
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-08-28 23:52:54 -0400

    Single threaded solution.

--- textbooks/os_concepts_galvin/osc9e_src/ch4/programming_problems/pp_4_22/prog_prob_4_22.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch4/programming_problems/pp_4_22/prog_prob_4_22.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
+#include <limits.h>
 
 /*
 
@@ -26,20 +27,75 @@
 */
 
 
-long get_rand_fraction(void) {
-    float frn;
-    float frd;
+double get_rand_fraction(void) {
+    double frn;
+    double frd;
 
-    frn = (float) rand();
-    frd = (float) RAND_MAX;
+    frn = (double) arc4random_uniform(UINT32_MAX);
+    frd = (double) UINT32_MAX;
 
 
-    frn = 10000000.0 * (frn/frd);
+    frn = (frn/frd);
 
-    return (long) frn;
+    return frn;
+}
+
+typedef struct _point_t {
+    double x;
+    double y;
+} point_t;
+
+void get_rand_point(point_t *p) {
+    p->x = -1.0 + 2.0 * get_rand_fraction();
+    p->y = -1.0 + 2.0 * get_rand_fraction();
+}
+
+int is_inside_circle(point_t *p){
+    double xs, ys, s;
+
+    xs = p->x * p->x;
+    ys = p->y * p->y;
+
+    s = sqrt(xs + ys);
+
+    return s < 1.0;
+}
+
+double inside = 0.0;
+
+void inside_count(long n) {
+    point_t p;
+
+    while (n-- > 0) {
+        get_rand_point(&p);
+        if (is_inside_circle(&p))
+            inside++;
+    }
 }
 
 int main(int argc, char **argv) {
-     sranddev(); // Use a fresh seed for rand().
+    long num_points;
+    double t;
+
+    if (argc <= 1 || argc > 2) {
+        printf("Usage: Provide 1 positive integer. Example: ./a.out 7.\n");
+        return -1;
+    }
+
+    num_points = strtol(argv[1], NULL, 10);
+
+    if (num_points < 1) {
+        printf("Failed to convert \"%s\" to a positive integer.\n", argv[1]);
+        return -1;
+    }
+
+    t = (double) num_points;
+
+    inside_count(num_points);
+
+    t = 4.0 * inside / t;
+
+    printf("Pi ~= %.12f. It should be close to 3.141592653589793.\n", t);
+
     return 0;
 }
