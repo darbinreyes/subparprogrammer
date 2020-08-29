50b57462e652968f719a126536e02f961d44d6b5
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-08-28 22:46:51 -0400

    Plan.

--- textbooks/os_concepts_galvin/osc9e_src/ch4/programming_problems/pp_4_22/prog_prob_4_22.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch4/programming_problems/pp_4_22/prog_prob_4_22.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long get_rand_sleep_ns(void) {
+
+/*
+
+    The unit circle described in the problem is has a radius of 1 and is
+    inscribed in a square whose sides equal 2. Therefore, to generate a
+    random x coordinate, we can multiply a random fraction by 2 (the size of the
+    square's side) and add the product to the lesser x coordinate, which is -1.
+    We can generate a random y coordinate in exactly the same way.
+
+    Next, we must determine if the randomly generated point falls within the
+    circle. Since the radius of the circle is 1, all we need to do is compute
+    the distance of the random point from the the origin (x,y) = (0,0), we can
+    use Pythagoras theorem for that, finally, if that distance is less than 1
+    we know that the point has fallen within the circle. We shall consider
+    points that are exactly a distance 1 from the origin to be outside of the
+    circle.
+
+    Finally, we compute the estimation of pi with:
+
+    4.0*num_inside/num_outside.
+
+
+*/
+
+
+long get_rand_fraction(void) {
     float frn;
     float frd;
-
 
     frn = (float) rand();
     frd = (float) RAND_MAX;
@@ -15,6 +39,7 @@ long get_rand_sleep_ns(void) {
     return (long) frn;
 }
 
-int main(int arc, char **argv) {
+int main(int argc, char **argv) {
+     sranddev(); // Use a fresh seed for rand().
     return 0;
 }
