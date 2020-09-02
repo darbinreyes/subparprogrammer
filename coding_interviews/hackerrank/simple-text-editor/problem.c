806fe8b5537998c77e884266ea42c7eaaf12716d
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-09-02 11:16:51 -0400

    Progress on hackerrank problem.

--- coding_interviews/hackerrank/simple-text-editor/problem.c
+++ coding_interviews/hackerrank/simple-text-editor/problem.c
@@ -9,8 +9,28 @@
 #include <math.h>
 #include <stdlib.h>
 
+/* Operation types.
+
+
+*/
+
+/* Constraints.
+
+    The sum of the lengths of all W in the input <= 10^6
+    =>
+    The length of the character buffer should be 10^6 chars.
+
+*/
+#define CHAR_BUF_SIZE 1000000
+static char cbuf[CHAR_BUF_SIZE]; // Stores characters currently in the editor.
+static int clen; // The number of characters current in cbuf.
+static char *cend; // Points to the next free spot in cbuf.
+
+
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+
     return 0;
 }
