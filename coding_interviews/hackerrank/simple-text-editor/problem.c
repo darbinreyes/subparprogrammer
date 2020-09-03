a6932bc37c81b11dffea66140551a8aa7dab07b4
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-09-03 14:30:34 -0400

    Problem progress.

--- coding_interviews/hackerrank/simple-text-editor/problem.c
+++ coding_interviews/hackerrank/simple-text-editor/problem.c
@@ -11,6 +11,10 @@
 
 /* Operation types.
 
+    1. append(W)
+    2. delete(k)
+    3. print(kth character)
+    4. undo() last 1 or 2 operation
 
 */
 
@@ -21,6 +25,25 @@
     The length of the character buffer should be 10^6 chars.
 
 */
+
+/* Scratch Work
+
+    The main difficulty in this problem is the implementation of the undo()
+    operation.
+
+    We can use a stack to store the information necessary to undo each #1 and
+    #2 operation. When an undo() is requested, can pop the stack and use its
+    contents to undo the operation. A struct to represent each stack item seems
+    appropriate. Since we must store the characters deleted by a delete()
+    operation to undo() it, which can be as many as 10^6 character, we can use
+    malloc() to allocate memory for those characters. Note that to undo() a
+    append(W) operation, we can perform a delete(W.length) operation. Similarly,
+    to undo() a delete(k) operation, we can perform an append(W) operation where
+    W = stack_item.chrs.
+
+*/
+
+
 #define CHAR_BUF_SIZE 1000000
 static char cbuf[CHAR_BUF_SIZE]; // Stores characters currently in the editor.
 static int clen; // The number of characters current in cbuf.
