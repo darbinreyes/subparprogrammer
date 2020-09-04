6310f5faaa29fdf9f5790d7b07374d7479186d6b
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-09-04 17:21:03 -0400

    Implement the first 3 operations.

--- coding_interviews/hackerrank/simple-text-editor/problem.c
+++ coding_interviews/hackerrank/simple-text-editor/problem.c
@@ -43,17 +43,105 @@
 
 */
 
+#include <assert.h>
 
-#define CHAR_BUF_SIZE 1000000
+#define CHAR_BUF_LEN 1000000
+#define CHAR_BUF_SIZE (CHAR_BUF_LEN+1)
+
 static char cbuf[CHAR_BUF_SIZE]; // Stores characters currently in the editor.
-static int clen; // The number of characters current in cbuf.
-static char *cend; // Points to the next free spot in cbuf.
+static int clen = 0; // The number of characters current in cbuf.
+static char *cend = cbuf; // Points to the next free spot in cbuf.
+
+
+/* Append the given characters. */
+void append(char *w) {
+    assert(w != NULL);
+    assert(clen < CHAR_BUF_SIZE);
+
+    while (*w != '\0') {
+        *cend = *w;
+        cend++;
+        clen++;
+    }
+}
+
+/* Delete k characters. */
+void delete(int k) {
+
+/*
+
+[A][]
+   ^cend
+
+clen = 1.
+
+# after delete(1)
+
+[A][]
+^cend
+
+clen = 0.
+
+
+
+*/
+    assert(k >= 0);
+
+    if (k == 0) // No-op.
+        return;
+
+/* If k is greater than or equal the number of characters in the buffer we just
+reset to the initial empty state. */
+
+    if (k >= clen) {
+        cend = cbuf;
+        clen = 0;
+        return;
+    }
+
+    while (clen > 0) {
+        cend--;
+        clen--;
+    }
+}
+
+/* Print the k-th character. */
+void print (int k) {
+    assert(k >= 0);
+
+/*
+
+[A][B][]...
+      ^cbuf
+clen = 2.
+
+print(0)
+
+# prints A
+
+print(1)
+
+# prints B
+
+print(2)
+
+# out of bounds.
+
+*/
+
+    if (k < clen) {
+        putchar(cbuf[k]);
+        putchar('\n');
+    } // else // k is out of bounds, treat is as a NO-OP.
+}
 
 
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
 
+    /* Initialize buffer pointer */
+//    cend = cbuf;
 
     return 0;
 }
