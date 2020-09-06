a9ccd4f330a6fe5960af5380782cdfe24f7d26c4
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-09-06 01:10:46 -0400

    Question 3.

--- c_projects/project1/boot_sector_6.asm
+++ c_projects/project1/boot_sector_6.asm
@@ -26,19 +26,34 @@ mov bx, 30
 ;
 ;if (bx <= 4) {
 ;    mov al, 'A'
-;} else if (bx) {
+;} else if (bx < 40) {
 ;    mov al, 'B'
 ;} else {
 ;    mov al, 'C'
 ;}
 ;
 
+cmp bx, 4
+jle print_a
 
+cmp bx, 40
+jl print_b
 
-mov ah, 0x0e ; int_0x10(ah=0x0e) BIOS scrolling teletype mode.
-int 0x10
+print_a:
+    mov al, 'A'
+    jmp done
 
-jmp $
+print_b:
+    mov al, 'B'
+    jmp done
+
+mov al, 'C'
+
+done:
+    mov ah, 0x0e ; int_0x10(ah=0x0e) BIOS scrolling teletype mode.
+    int 0x10
+
+jmp $ ; Infinite loop.
 
 ; 0 padding to 512 bytes.
 times 510-($-$$) db 0 ; db = declare byte.
