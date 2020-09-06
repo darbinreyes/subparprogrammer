251ce9440f6766dbc4a8febba5ca053f02686e44
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-09-06 01:02:45 -0400

    Starting question 3.

---
+++ c_projects/project1/boot_sector_6.asm
@@ -0,0 +1,46 @@
+;
+; Section 3.4.5 Control Structures. Question 3.
+; Remarks:
+; * Translating C if-elseif-else into x86.
+; * In writing assembly it is useful to start with C code and then translate it
+;   into instructions/x86.
+; * Using the cmp instruction and conditional jumps.
+; * cmp x, y
+;   * x </<=/>/>=/==/!= y.
+;
+
+mov bx, 30
+
+;
+; Test cases.
+;
+; [] bx == 30, prints B.
+; [] bx == 39, prints B.
+; [] bx == 4, prints A.
+; [] bx == 3, prints A.
+; [] bx == 40, prints C.
+; [] bx == 40, prints C.
+;
+
+; pseudo C code to translate into x86.
+;
+;if (bx <= 4) {
+;    mov al, 'A'
+;} else if (bx) {
+;    mov al, 'B'
+;} else {
+;    mov al, 'C'
+;}
+;
+
+
+
+mov ah, 0x0e ; int_0x10(ah=0x0e) BIOS scrolling teletype mode.
+int 0x10
+
+jmp $
+
+; 0 padding to 512 bytes.
+times 510-($-$$) db 0 ; db = declare byte.
+
+dw 0xaa55 ; BIOS boot sector magic number.
