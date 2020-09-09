eb0952d516f76760b76709000bf7121af70fec35
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-09-09 18:20:19 -0400

    Question 5 completed.

--- c_projects/project1/boot_sector_8.asm
+++ c_projects/project1/boot_sector_8.asm
@@ -11,39 +11,53 @@ call print_hex ; call the function
 jmp $ ; Infinite loop.
 
 ; prints the value of DX as hex.
-print_hex:
+print_hex: ; FYI: This routine always converts and prints 4 nibbles, even if e.g. dx == 0x0000.
     ; TODO : manipulate chars at HEX_OUT to reflect DX
 pusha ; save register values
-and dx, 0x000f
+
+mov bx, HEX_OUT ; Initialize HEX_OUT pointer
+add bx, 5
+mov ax, 0 ; Nibble's printed count.
+
+
+next_nibble:
+push dx
+and dx, 0x000f ; Get the lower nibble
 cmp dx, 10
-jl is_decimal
+jl is_digit
 
 cmp dx, 16
 jl is_hex
 
-popa
-ret ; Out of nibble range. Should not occur.
+jmp print_hex_done ; Nibble is out of range. Should not occur.
 
-is_hex:
+is_hex: ; Store 'a'-'f' in cl
 mov cx, dx
 sub cx, 10
-add cx, 'a' ; cx contains our character.
+add cx, 'a' ; cl contains our ASCII character.
 jmp update_str
 
-is_decimal:
+is_digit: ; Store '0'-'9' in cl
 mov cx, dx
-add cx, '0' ; cx contains our character.
+add cx, '0' ; cl contains our ASCII character.
 jmp update_str
 
 update_str:
-mov bx, HEX_OUT
-add bx, 5
-mov dx, [bx]
-mov dl, cl
-mov [bx], dx
+mov dx, [bx] ; Load before store to preserve the most significant byte.
+mov dl, cl   ; cl contains our ASCII character.
+mov [bx], dx ; Store ASCII character.
+
+pop dx    ; Prepare for the next nibble.
+shr dx, 4
+sub bx, 1 ; Update HEX_OUT pointer.
+add ax, 1
+cmp ax, 4
+jl next_nibble
 
 mov bx, HEX_OUT ; print the string pointed to
 call print_string ; by BX
+
+print_hex_done:
 
 popa ; restore register values
 ret
