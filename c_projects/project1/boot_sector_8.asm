14c54215cc332ae20fa737c8d4a3f7c77d9a0c7b
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-09-09 15:28:00 -0400

    Step 1. Just print the template string.

--- c_projects/project1/boot_sector_8.asm
+++ c_projects/project1/boot_sector_8.asm
@@ -1,8 +1,14 @@
 ;
 ; Question 5. Implementation print_hex.
 ;
+
+[org 0x7c00] ; Tell assembler where we will be loaded into memory.
+
 mov dx, 0x1fb6 ; store the value to print in dx
 call print_hex ; call the function
+
+
+jmp $ ; Infinite loop.
 
 ; prints the value of DX as hex.
 print_hex:
@@ -12,5 +18,12 @@ mov bx, HEX_OUT ; print the string pointed to
 call print_string ; by BX
 ret
 
+%include "print_string.asm"
+
 ; global variables
 HEX_OUT: db '0x0000', 0
+
+; 0 padding and magic number.
+
+times 510-($-$$) db 0
+dw 0xaa55
