71168c935d7b5180c61a1e2bf4e64b19bd85a203
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-09-09 23:30:05 -0400

    Given code.

--- c_projects/project1/boot_sector_9.asm
+++ c_projects/project1/boot_sector_9.asm
@@ -7,3 +7,33 @@
 ; = (0xffff*16 + 0xffff) + 1
 ; = (0xffff0 + 0xffff) + 1 = (0x10ffef) + 1 = 0x10fff0.
 ;
+
+mov ah, 0x0e ; int 0x10, ah = 0x0e, scrolling teletype BIOS routine.
+
+mov al, [the_secret]
+int 0x10 ; Does this print an X?
+
+mov bx, 0x7c0 ; We can't load immediates directly into the segment registers.
+              ; This achieves an effect equivalent to using the [org 0x7c00]
+              ; assembler directive.
+mov ds, bx
+mov al, [the_secret] ; The ds segment register is implied here.
+int 0x10 ; X?
+
+mov al, [es:the_secret] ; Explicitly specify that the es segment register be
+                        ; used for addressing.
+int 0x10 ; X?
+
+mov bx, 0x7c0 ; See same instruction above.
+mov es, bx
+mov al, [es:the_secret]
+int 0x10 ; X?
+
+jmp $ ; Infinite loop.
+
+the_secret:
+    db 'X' ; Declare byte.
+
+; 0 padding and magic BIOS number.
+times 510-($-$$) db 0
+dw 0xaa55
