8567045d771a015c0d59d0ae4a8a858f60f768c0
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-09-18 02:00:01 -0400

    Yeah Buddy! About to implement and execute my first kernel bruh.

---
+++ c_projects/project1/kernel_0.c
@@ -0,0 +1,7 @@
+// Print a single character to via direct write to video memory.
+void main(void) {
+    unsigned char *video_memory = (unsigned char *) 0x000b8000;
+    *video_memory = 'X';
+    //*(video_memory + 1) = 0x0f; // White on black.
+    return;
+}
