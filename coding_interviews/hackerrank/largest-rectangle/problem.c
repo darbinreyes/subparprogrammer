99705173fe2def4f92f01e0d3ca320754bee1a7d
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-10-01 17:24:27 -0400

    Problem notes.

--- coding_interviews/hackerrank/largest-rectangle/problem.c
+++ coding_interviews/hackerrank/largest-rectangle/problem.c
@@ -6,6 +6,39 @@
 
 */
 
+/*
+
+demolish a number of old
+construct a shopping mall in their place
+Your task is to **find** the largest solid **area** in which the mall can be constructed.
+
+**two**-dimensional landscape.
+
+height, given by =
+`h[i]`
+`i in [1, n]`
+
+if **join**
+`k `
+adjacent buildings,
+
+form a solid rectangle of area
+= k *
+min(
+h[i]
+h[i+1]
+...
+h[i+k-1]
+)
+
+Example:
+h=[3,2,3]->rectangle <height,length> = <h, k> = <2,3>
+area = h * k = 6.
+
+
+
+*/
+
 #include <stdlib.h>
 
 int main(void) {
