6beabae929b1a52c9a6d9a0e65f321ac34caed12
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-10-01 17:24:17 -0400

    Test the identify keyboard command.

--- c_projects/project1/drivers/keyboard.h
+++ c_projects/project1/drivers/keyboard.h
@@ -2,5 +2,7 @@
 #define __KEYBOARD_H__
 
 int PS_2_controller_get_status_register(unsigned char *ctlr_stat);
-
+unsigned char send_disable_kbd_cmd (void);
+unsigned char send_kbd_cmd (unsigned char cmd);
+unsigned char receive_kbd_byte (void);
 #endif
