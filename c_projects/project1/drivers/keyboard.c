7bf3ee9346b6030ebacb233905c3e86c40017c19
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-10-02 01:46:53 -0400

    Cleanup a bit. Working on header.

--- c_projects/project1/drivers/keyboard.c
+++ c_projects/project1/drivers/keyboard.c
@@ -8,10 +8,7 @@
 
 #include "../kernel/low_level.h"
 #include "../mylibc/mylibc.h"
-
-#define IO_PORT_PS_2_CTLR_DATA          0x60 // CTLR = ConTroLleR.
-#define IO_PORT_PS_2_CTLR_STAT_REGISTER 0x64
-#define IO_PORT_PS_2_CTLR_CMD_REGISTER  0x64
+#include "keyboard.h"
 
 /*
 
@@ -23,30 +20,6 @@
 int PS_2_controller_port1_get_device_ID (unsigned char *id) {
     return 0;
 }
-
-
-typedef
-struct _ps2_ctrl_stat_t {
-/*
-
-    PS/2 controller status register bit definitions.
-
-    [Documentation](https://wiki.osdev.org/%228042%22_PS/2_Controller#Status_Register).
-
-    * I/O naming convention is relative to the PS/2 device.
-      * Output means data flow from PS/2 device to the CPU. (PS/2 device -> CPU).
-      * Input means data flow from CPU to the PS/2 device. (PS/2 device <- CPU).
-
-*/
-unsigned char obuf_is_full:1;
-unsigned char ibuf_is_full:1;
-unsigned char sys_fw_post_passed:1;
-unsigned char ibuf_dev_or_ctlr:1;
-unsigned char rsvd:2;
-unsigned char time_out_err:1;
-unsigned char parity_err:1;
-} ps2_ctrl_stat_t;
-
 
 /*
 
@@ -66,14 +39,10 @@ int PS_2_controller_get_status_register(unsigned char *ctlr_stat) {
     return 0;
 }
 
+/*
 
-#define DISABLE_KBD_CMD (0xF5)
-#define IBUF_IS_FULL (1)
-#define IBUF_IS_EMPTY (0)
-#define OBUF_IS_FULL (1)
-#define OBUF_IS_EMPTY (0)
-#define IDENTIFY_DEVICE_CMD (0xF2)
 
+*/
 unsigned char send_disable_kbd_cmd (void) {
     unsigned char resp;
     unsigned char st_reg;
@@ -86,7 +55,7 @@ unsigned char send_disable_kbd_cmd (void) {
     timeout_counter = 1000000;
     r = PS_2_controller_get_status_register(&st_reg);
 
-    while (timeout_counter > 0 && ( (st_reg & BIT1) == IBUF_IS_FULL ) ) {
+    while (timeout_counter > 0 && ( (st_reg & BIT1) == BUF_IS_FULL ) ) {
         r = PS_2_controller_get_status_register(&st_reg);
         timeout_counter--;
     }
@@ -95,7 +64,7 @@ unsigned char send_disable_kbd_cmd (void) {
         return 1;
     }
 
-    if (( (st_reg & BIT1) == IBUF_IS_FULL )) {
+    if (( (st_reg & BIT1) == BUF_IS_FULL )) {
         return 2;
     }
 
@@ -108,7 +77,7 @@ unsigned char send_disable_kbd_cmd (void) {
     timeout_counter = 1000000;
     r = PS_2_controller_get_status_register(&st_reg);
 
-    while (timeout_counter > 0 && ( (st_reg & BIT0) == OBUF_IS_EMPTY ) ) {
+    while (timeout_counter > 0 && ( (st_reg & BIT0) == BUF_IS_EMPTY ) ) {
         r = PS_2_controller_get_status_register(&st_reg);
         timeout_counter--;
     }
@@ -120,7 +89,10 @@ unsigned char send_disable_kbd_cmd (void) {
 }
 
 
+/*
 
+
+*/
 unsigned char send_kbd_cmd (unsigned char cmd) {
     unsigned char resp;
     unsigned char st_reg;
@@ -133,7 +105,7 @@ unsigned char send_kbd_cmd (unsigned char cmd) {
     timeout_counter = 1000000;
     r = PS_2_controller_get_status_register(&st_reg);
 
-    while (timeout_counter > 0 && ( (st_reg & BIT1) == IBUF_IS_FULL ) ) {
+    while (timeout_counter > 0 && ( (st_reg & BIT1) == BUF_IS_FULL ) ) {
         r = PS_2_controller_get_status_register(&st_reg);
         timeout_counter--;
     }
@@ -142,7 +114,7 @@ unsigned char send_kbd_cmd (unsigned char cmd) {
         return 1;
     }
 
-    if (( (st_reg & BIT1) == IBUF_IS_FULL )) {
+    if (( (st_reg & BIT1) == BUF_IS_FULL )) {
         return 2;
     }
 
@@ -155,7 +127,7 @@ unsigned char send_kbd_cmd (unsigned char cmd) {
     timeout_counter = 1000000;
     r = PS_2_controller_get_status_register(&st_reg);
 
-    while (timeout_counter > 0 && ( (st_reg & BIT0) == OBUF_IS_EMPTY ) ) {
+    while (timeout_counter > 0 && ( (st_reg & BIT0) == BUF_IS_EMPTY ) ) {
         r = PS_2_controller_get_status_register(&st_reg);
         timeout_counter--;
     }
@@ -164,7 +136,7 @@ unsigned char send_kbd_cmd (unsigned char cmd) {
         return 3;
     }
 
-    if (( (st_reg & BIT0) == OBUF_IS_EMPTY )) {
+    if (( (st_reg & BIT0) == BUF_IS_EMPTY )) {
         return 4;
     }
 
@@ -174,7 +146,11 @@ unsigned char send_kbd_cmd (unsigned char cmd) {
 
 }
 
+/*
 
+
+
+*/
 unsigned char receive_kbd_byte (void) {
     unsigned char resp;
     unsigned char st_reg;
@@ -187,7 +163,7 @@ unsigned char receive_kbd_byte (void) {
     timeout_counter = 1000000;
     r = PS_2_controller_get_status_register(&st_reg);
 
-    while (timeout_counter > 0 && ( (st_reg & BIT0) == OBUF_IS_EMPTY ) ) {
+    while (timeout_counter > 0 && ( (st_reg & BIT0) == BUF_IS_EMPTY ) ) {
         r = PS_2_controller_get_status_register(&st_reg);
         timeout_counter--;
     }
@@ -196,7 +172,7 @@ unsigned char receive_kbd_byte (void) {
         return 1;
     }
 
-    if (( (st_reg & BIT0) == OBUF_IS_EMPTY )) {
+    if (( (st_reg & BIT0) == BUF_IS_EMPTY )) {
         return 2;
     }
 
