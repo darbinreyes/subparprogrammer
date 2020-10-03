188e6053c29845bf0ed89bae5beb5aef9213ea23
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-10-03 07:10:53 -0400

    Add keyboard commands.

--- c_projects/project1/drivers/keyboard.h
+++ c_projects/project1/drivers/keyboard.h
@@ -20,6 +20,33 @@ Commands that can be sent to a PS/2 device (e.g. keyboard). This driver assumes 
 #define DISABLE_KBD_CMD (0xF5)
 #define IDENTIFY_DEVICE_CMD (0xF2)
 
+typedef
+enum _ps2_kbd_cmd {
+// If expects data, send after command.
+// IMPORTANT: May have to insert a wait time before sending expected data. And before checking for the response.
+// "Response: default" means "Response: 0xFA=ACK/0xFE=RESEND.""
+SET_LEDS = 0xED, // Expects Data: Yes. Response: default.
+ECHO = 0xEE, // Expects Data: No. Response: 0xEE=echo/0xFE=RESEND.
+GET_SET_SCAN_CODE_SET = 0xF0, // Expects Data: Yes. 0x00=Get scan code set number. Response:  ACK, then the scan code set number. // See spec. for other responses.
+IDENTIFY_KBD = 0xF2, // Expects Data: No. Response:  ACK, then 0, 1, or 2 bytes identifying the keyboard. // [x] Tested, received (0xAB, 0x41) = "MF2 keyboard with translation enabled in the PS/2 controller". // See spec. for other responses, e.g. 0x00="standard PS/2 mouse".
+// TODO: "typematic rate and delay", "make/release", "autorepeat" related commands.
+ENABLE_SCANNING = 0xF4, // Expects Data: No. Response: default.
+DISABLE_SCANNING = 0xF5, // Expects Data: No. Response: default.
+SET_DEFAULTS = 0xF6, // Expects Data: No. Response: default.
+RESEND_CMD = 0xFE, // Expects Data: No. Response: 0xXX="Previously sent byte"/0xFE=RESEND.
+RESET_AND_SELF_TEST = 0xFF // Expects Data: No. Response: 0xAA=self-test passed, 0xFC||0xFD=self-test failed, 0xFE=RESEND.
+} ps2_kbd_cmd;
+
+typedef
+enum _ps2_kbd_rsp_t {
+KEY_DETECT_OR_BUF_ERR0 = 0x00,
+SELF_TEST_PASSED = 0xAA,
+ECHO_RSP = 0xEE,
+ACK = 0xFA,
+SELF_TEST_FAILED = 0xFC, // [] Test // else response may be 0xFD.
+RESEND_RSP = 0xFE, // [] TODO: If used, limit retries to 3.
+KEY_DETECT_OR_BUF_ERR0 = 0xFF
+} ps2_kbd_rsp_t;
 
 /*
     resp = send_kbd_cmd (0xF5); // Disable scanning
