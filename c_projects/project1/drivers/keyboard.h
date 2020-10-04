dc88f10bfcfc94059aad4d9139b955ceeb1bf43e
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-10-04 00:52:38 -0400

    Cleanup. Move some code from kbd to ps2. Rename rcv_ctlr_stat(). Testing commands in progress.

--- c_projects/project1/drivers/keyboard.h
+++ c_projects/project1/drivers/keyboard.h
@@ -9,21 +9,40 @@ Commands that can be sent to a PS/2 keyboard.
 
 */
 typedef
-enum _ps2_kbd_cmd {
+enum _ps2_kbd_cmd_t {
 // If expects data, send after command.
 // IMPORTANT: May have to insert a wait time before sending expected data. And before checking for the response.
 // "Response: default" means "Response: 0xFA=ACK/0xFE=RESEND.""
 SET_LEDS = 0xED, // Expects Data: Yes. Response: default.
-ECHO = 0xEE, // Expects Data: No. Response: 0xEE=echo/0xFE=RESEND.
-GET_SET_SCAN_CODE_SET = 0xF0, // Expects Data: Yes. 0x00=Get scan code set number. Response:  ACK, then the scan code set number. // See spec. for other responses.
-IDENTIFY_KBD = 0xF2, // Expects Data: No. Response:  ACK, then 0, 1, or 2 bytes identifying the keyboard. // [x] Tested, received (0xAB, 0x41) = "MF2 keyboard with translation enabled in the PS/2 controller". // See spec. for other responses, e.g. 0x00="standard PS/2 mouse".
-// TODO: "typematic rate and delay", "make/release", "autorepeat" related commands.
-ENABLE_SCANNING = 0xF4, // Expects Data: No. Response: default.
-DISABLE_SCANNING = 0xF5, // Expects Data: No. Response: default.
+ECHO = 0xEE, // [x] Tested. Expects Data: No. Response: 0xEE=echo/0xFE=RESEND.
+/*
+
+If translation is enabled the controller will translate "scan code set 2" into "scan code set 1".
+// [x] Tested. Expects Data: Yes. 0x00=Get scan code set number. Response:  ACK, then the scan code set number. // See spec. for other responses.
+
+*/
+GET_SET_SCAN_CODE_SET = 0xF0,
+IDENTIFY_KBD = 0xF2, // [x] Tested. Expects Data: No. Response:  ACK, then 0, 1, or 2 bytes identifying the keyboard. // [x] Tested, received (0xAB, 0x41) = "MF2 keyboard with translation enabled in the PS/2 controller". // See spec. for other responses, e.g. 0x00="standard PS/2 mouse".
+// TODO?: "typematic rate and delay", "make/release", "autorepeat" related commands.
+ENABLE_SCANNING = 0xF4, // [x] Tested. Expects Data: No. Response: default.
+DISABLE_SCANNING = 0xF5, // [x] Tested. Expects Data: No. Response: default.
 SET_DEFAULTS = 0xF6, // Expects Data: No. Response: default.
 RESEND_CMD = 0xFE, // Expects Data: No. Response: 0xXX="Previously sent byte"/0xFE=RESEND.
 RESET_AND_SELF_TEST = 0xFF // Expects Data: No. Response: 0xAA=self-test passed, 0xFC||0xFD=self-test failed, 0xFE=RESEND.
-} ps2_kbd_cmd;
+} ps2_kbd_cmd_t;
+
+/*
+
+Some PS/2 keyboard commands require data to be sent after the command is sent.
+
+*/
+typedef
+enum _ps2_kbd_cmd_data_t {
+    GET_SCAN_CODE_SET = 0x00,
+    SET_SCAN_CODE_SET_1 = 0x01,
+    SET_SCAN_CODE_SET_2 = 0x02,
+    SET_SCAN_CODE_SET_3 = 0x03
+}  ps2_kbd_cmd_data_t;
 
 /*
 
@@ -40,12 +59,5 @@ SELF_TEST_FAILED = 0xFC, // [] Test // else response may be 0xFD.
 RESEND_RSP = 0xFE, // [] TODO: If used, limit retries to 3.
 KEY_DETECT_OR_BUF_ERR_1 = 0xFF
 } ps2_kbd_rsp_t;
-
-
-// unsigned char send_kbd_cmd (unsigned char cmd);
-unsigned char send_cmd (unsigned char cmd);
-
-//unsigned char receive_kbd_byte (void);
-unsigned char rcv_data (void);
 
 #endif
--- c_projects/project1/drivers/ps_2_ctlr.c
+++ c_projects/project1/drivers/ps_2_ctlr.c
@@ -1,5 +1,9 @@
 #include "../mylibc/mylibc.h"
+#include "../kernel/low_level.h"
 #include "ps_2_ctlr.h"
+
+// TODO: driver start()
+// TODO: driver stop()
 
 /*
 
@@ -10,7 +14,7 @@
     @retval 0 Successful. The status has been returned in cntlr_stat.
 
 */
-int rcv_ctlr_stat(unsigned char *ctlr_stat) {
+int get_ctlr_stat(unsigned char *ctlr_stat) {
     // TODO: Use bit packed structure.
     if (ctlr_stat == NULL)
         return -1;
@@ -19,3 +23,94 @@ int rcv_ctlr_stat(unsigned char *ctlr_stat) {
     return 0;
 }
 
+/*
+
+
+*/
+unsigned char send_cmd (unsigned char cmd) {
+    unsigned char resp;
+    unsigned char st_reg;
+    int timeout_counter;
+    int r;
+
+    // SEND.
+    // Send the KBD a disable command (Stop sending scan codes/ignore the user).
+
+    timeout_counter = 1000000;
+    r = get_ctlr_stat(&st_reg);
+
+    while (timeout_counter > 0 && ( (st_reg & BIT1) == 1 ) ) {
+        r = get_ctlr_stat(&st_reg);
+        timeout_counter--;
+    }
+
+    if (timeout_counter == 0) {
+        return 1;
+    }
+
+    if (( (st_reg & BIT1) == 1 )) {
+        return 2;
+    }
+
+    port_byte_out (IO_PS2_CTLR_DATA, cmd); // Disable KBD command.
+
+
+    // RECEIVE.
+    // Wait from ACK response from KBD.
+
+    timeout_counter = 1000000;
+    r = get_ctlr_stat(&st_reg);
+
+    while (timeout_counter > 0 && ( (st_reg & BIT0) == 0 ) ) {
+        r = get_ctlr_stat(&st_reg);
+        timeout_counter--;
+    }
+
+    if (timeout_counter == 0) {
+        return 3;
+    }
+
+    if (( (st_reg & BIT0) == 0 )) {
+        return 4;
+    }
+
+    resp = port_byte_in (IO_PS2_CTLR_DATA);
+
+    return resp;
+
+}
+
+/*
+
+
+
+*/
+unsigned char rcv_data (void) {
+    unsigned char resp;
+    unsigned char st_reg;
+    int timeout_counter;
+    int r;
+
+    // RECEIVE.
+    // Wait from ACK response from KBD.
+
+    timeout_counter = 1000000;
+    r = get_ctlr_stat(&st_reg);
+
+    while (timeout_counter > 0 && ( (st_reg & BIT0) == 0 ) ) {
+        r = get_ctlr_stat(&st_reg);
+        timeout_counter--;
+    }
+
+    if (timeout_counter == 0) {
+        return 1;
+    }
+
+    if (( (st_reg & BIT0) == 0 )) {
+        return 2;
+    }
+
+    resp = port_byte_in (IO_PS2_CTLR_DATA);
+
+    return resp;
+}
