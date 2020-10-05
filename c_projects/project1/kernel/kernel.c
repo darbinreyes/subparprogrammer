1b1d2ffb83559097e6fa67153f8ff2c49f6a98f6
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-10-04 21:43:56 -0400

    Test reading a scan code.

--- c_projects/project1/kernel/kernel.c
+++ c_projects/project1/kernel/kernel.c
@@ -9,6 +9,8 @@ int main(void) {
     int r;
     unsigned char stat;
     unsigned char b;
+    unsigned char sc;
+    char c;
 
     clear_screen();
     print_at("Edsger Dijkstra!\n", 0, 0);
@@ -23,21 +25,29 @@ int main(void) {
     print_byteb (stat);
     print("\n");
 
-    r = send_byte (DISABLE_SCANNING); // Disable scanning
+    r = get_scan_code(&sc);
 
     if (r != 0) {
         print("Error 1.\n");
         return 1;
     }
-    r = rcv_byte (&b);
 
-    if (r != 0) {
-        print("Error 2.\n");
-        return 1;
-    }
-
-    print_byteh (b);
+    print_byteh(sc);
     print("\n");
+
+    while(1) {
+        r = get_scan_code(&sc);
+
+        if (r != 0) {
+            print("Error 2.\n");
+            return 1;
+        }
+
+        c = scan_code_to_ascii (sc);
+
+        print_ch_at(c, 0, -1, -1);
+        print("\n");
+    }
 
     return 0;
 }
