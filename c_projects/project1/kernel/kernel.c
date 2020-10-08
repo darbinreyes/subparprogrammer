04dc9e288270a918fb036a811f51d96231158db8
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-10-08 01:51:49 -0400

    Test PS/2 controller status register using bit packed struct.

--- c_projects/project1/kernel/kernel.c
+++ c_projects/project1/kernel/kernel.c
@@ -12,6 +12,7 @@ int main(void) {
     unsigned char sc;
     unsigned char sc2[2];
     char c;
+    ps_2_ctrl_stat_t stat2;
 
     clear_screen();
     print_at("Edsger Dijkstra!\n", 0, 0);
@@ -26,61 +27,76 @@ int main(void) {
     print_byteb (stat);
     print("\n");
 
-    // r = get_scan_code(&sc);
+    print("bit packed struct. controller status register.\n");
 
-    // if (r != 0) {
-    //     print("Error 1.\n");
-    //     return 1;
-    // }
+    while (1) {
+    r = get_ctlr_stat((unsigned char *)&stat2); // Read status register
 
-    // print_byteh(sc);
-    // print("\n");
+        print("0b");
+
+        if (stat2.par_err)
+            print("1");
+        else
+            print("0");
+
+        if (stat2.rcv_timeout)
+            print("1");
+        else
+            print("0");
+
+        if (stat2.tx_timeout)
+            print("1");
+        else
+            print("0");
+
+        if (stat2.dev_inhibited)
+            print("1");
+        else
+            print("0");
+
+        if (stat2.data_or_cmd)
+            print("1");
+        else
+            print("0");
+
+        if (stat2.sys_flg)
+            print("1");
+        else
+            print("0");
+
+        if (stat2.ibuf_full)
+            print("1");
+        else
+            print("0");
+
+        if (stat2.obuf_full)
+            print("1\n");
+        else
+            print("0\n");
+    }
 
     // while(1) {
-    //     r = get_scan_code(&sc);
+    //     r = get_scan_code2(sc2);
 
     //     if (r != 0) {
     //         print("Error 2.\n");
     //         return 1;
     //     }
 
-    //     if ( (sc & BIT7) == 0) { // Only print scan codes indicating press (not released).
+    //     if (sc2[1] != 0x00) {
+    //         print(" 2 byte sc.");
     //         print("[");
-    //         print_byteh(sc);
+    //         print_byteh(sc2[0]);
     //         print("]");
-    //         //c = scan_code_to_ascii (sc);
-
-    //         //print_ch_at(c, 0, -1, -1);
-    //         //print("\n");
+    //         print("[");
+    //         print_byteh(sc2[1]);
+    //         print("]");
     //     } else {
-    //         print("(");
-    //         print_byteh(sc);
-    //         print(")");
+    //         print(" 1 byte sc.\n");
+    //         print("[");
+    //         print_byteh(sc2[0]);
+    //         print("]");
     //     }
     // }
-
-    while(1) {
-        r = get_scan_code2(sc2);
-
-        if (r != 0) {
-            print("Error 2.\n");
-            return 1;
-        }
-
-        if (sc2[1] != 0x00) {
-            print(" 2 byte sc.");
-            print("[");
-            print_byteh(sc2[0]);
-            print("]");
-            print("[");
-            print_byteh(sc2[1]);
-            print("]");
-        } else {
-            print(" 1 byte sc.\n");
-            print("[");
-            print_byteh(sc2[0]);
-            print("]");
-        }
-    }
     return 0;
 }
