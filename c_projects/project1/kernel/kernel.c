7744b7c8543cb06f8afc92c4ebd633dc6c879fff
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2020-10-09 05:15:15 -0400

    Messy notes on how to verify the various integral type sizes.

--- c_projects/project1/kernel/kernel.c
+++ c_projects/project1/kernel/kernel.c
@@ -5,12 +5,35 @@
 #include "../drivers/keyboard.h"
 #include "../mylibc/mylibc.h"
 
+/* [Index of Directives](https://gcc.gnu.org/onlinedocs/cpp/Index-of-Directives.html#Index-of-Directives)
+    [] How to ensure my integers are of the size I expect?
+       How can I implement my own version the "C99 <inttypes.h> <stdint.h>"?
+
+    // uint16_t tw; // Generates compiler error "unknown type".
+    #if sizeof int != 4 // Does not work. "The preprocessor does not know anything about types in the language. Therefore, sizeof operators are not recognized in ‘#if’"
+
+    A preprecessor NO-OP = // "‘#’ followed by a newline, with only whitespace (including comments) in between. A null directive is understood as a preprocessing directive but has no effect on the preprocessor output. "
+
+    Standard pre-defined macros e.g.:
+        __FILE__
+        __LINE__
+
+    In conclusion, the int sizes should be tested at run time using size of. The target architecture determines the integer sizes, so as long as you are only targeting a single architecture you only need to check the sizes once.
+*/
+
+
+#if 0 == 1
+    #error "caca" // "The directive ‘#error’ causes the preprocessor to report a fatal error."
+    // #warning "pipi" // Just a warning, not fatal.
+#endif
+
 int main(void) {
     int r;
     unsigned char b;
     unsigned char sc[2];
     char c;
     ps_2_ctrl_stat_t stat;
+
 
     clear_screen();
     print_at("Edsger Dijkstra!\n", 0, 0);
