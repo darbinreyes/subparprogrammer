561a0f7f5d9beefeecae95e067019c64aa67a1c4
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-01-14 22:38:48 -0500

    Progress on the OPT page replacement algorithm.

--- textbooks/os_concepts_galvin/osc9e_src/ch9/programming_problems/pp_9_39/arr.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch9/programming_problems/pp_9_39/arr.c
@@ -5,13 +5,8 @@
 */
 
 #include <stdlib.h>
+#include <assert.h>
 #include "arr.h"
-
-typedef struct _arr_t {
-    int *arr;
-    int arr_len;
-    int num_entries;
-} arr_t;
 
 /*!
     @function alloc_arr
@@ -60,9 +55,83 @@ int alloc_arr(arr_t *a, int arr_len) {
 */
 int free_arr(arr_t *a) {
 
-    assert(a != NULL);
+    assert(a != NULL && a->arr != NULL);
 
-    if(a == NULL) {
+    if(a == NULL || a->arr == NULL) {
         return 1;
     }
+
+    free(a->arr);
+
+    return 0;
+}
+
+/*!
+    @function arr_contains
+
+    @discussion Tests if entry e is in the array. The search terminates as soon
+    as the first instance of e is found. Only entries inside of a->arr[0] to
+    a->arr[a->num_entries - 1] are searched.
+
+    @param a    Pointer to an arr_t
+
+    @param e    The entry to search for.
+
+    @result 1 if e is found in the array, 0 if e is not found in the array.
+    Other values indicate error.
+
+*/
+int arr_contains(arr_t *a, int e) {
+    int i;
+
+    assert(a != NULL && a->arr != NULL && a->num_entries >= 0);
+
+    if (a == NULL || a->arr == NULL || a->num_entries < 0) {
+        return -1;
+    }
+
+    for (i = 0; i < a->num_entries; i++) {
+        if(a->arr[i] == e)
+            return 1;
+    }
+
+    return 0;
+}
+
+/*!
+    @function  arr_add
+
+    @discussion Add an entry to the array if there is space.
+
+    @param a    Pointer to an arr_t
+
+    @param e    Entry to add to the array.
+
+    @result 0 if the entry was successfully added. Other values indicate an
+    error.
+*/
+int arr_add(arr_t *a, int e) {
+
+    assert(a != NULL && a->arr != NULL);
+
+    if(a == NULL || a->arr == NULL) {
+        return 1;
+    }
+
+    if(a->arr_len <= 0) {
+        return 2;
+    }
+
+    if(a->num_entries < 0) {
+        return 3;
+    }
+
+    if (a->num_entries >= a->arr_len) {
+        return 4; // No space
+    }
+
+    a->arr[a->num_entries] = e;
+    a->num_entries++;
+
+    return 0;
 }
