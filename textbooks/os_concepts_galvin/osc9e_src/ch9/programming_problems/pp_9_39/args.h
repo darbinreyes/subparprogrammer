57412392341a87c97dd99b936e380dd2292deb32
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-01-20 19:31:52 -0500

    Add support for -rand arg.

--- textbooks/os_concepts_galvin/osc9e_src/ch9/programming_problems/pp_9_39/args.h
+++ textbooks/os_concepts_galvin/osc9e_src/ch9/programming_problems/pp_9_39/args.h
@@ -2,5 +2,6 @@
 #define __ARGS_H__
 
 int get_arg_pi(const int argc, const char * const * const argv, int * const pi, const char * const arg_name);
+int get_arg_bool(const int argc, const char * const * const argv, int * const b, const char * const arg_name);
 
 #endif
