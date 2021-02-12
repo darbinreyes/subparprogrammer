f24697ce1bd5e4993e2e43bcc3eaff95cb222fbc
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-02-11 22:10:03 -0500

    Implement and test code to obtain the virtual addresses from addresses.txt with robust error checking. The project does not require any robust error checking, doing it just to exercise my error checking brain cells using the standard lib. functions.

--- textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/project_1_part_1.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/project_1_part_1.c
@@ -7,6 +7,7 @@
     * Given a file containing **logical addresses**.
     * We will simulate the use of a TLB and a page table.
     * Each logical address will be translated to its physical address.
+
 Specifics
 ===
     * The given file containing logical addresses is a list of
@@ -15,9 +16,12 @@ Specifics
     * The 16-bit values are to be divided into an 8-bit page number and an 8-bit
       page offset: viz. bits[15:8] = page number bits[7:0] = offset.
     * Other specifics:
-    * 2^8 entries in the page table. (= size_of_virtual_address_space/size_of_a_page = 2^16/2^8 = 2^8 = 256).
-    * Page size of 2^8 bytes. (= 2^(number_of_bits_in_page_offset_field_of_logical_address))
-    * A TLB with space for 16 entries. (=2^4). (Key=Page Number, Value=Frame Number).
+    * 2^8 entries in the page table.
+      (= size_of_virtual_address_space/size_of_a_page = 2^16/2^8 = 2^8 = 256).
+    * Page size of 2^8 bytes.
+      (= 2^(number_of_bits_in_page_offset_field_of_logical_address)).
+    * A TLB with space for 16 entries. (=2^4).
+      (Key=Page Number, Value=Frame Number).
     * Frame size of 2^8 bytes. (frame size always = page size = 2^8 bytes).
     * 256 frames. (This implies that the physical address space =
       number_of_frames * frame_size = 2^8 * 2^8 = 2^16 bytes, therefore the
@@ -25,6 +29,7 @@ Specifics
     * Physical memory of 65536 bytes = 2^16 bytes. (Hence above bullet point).
     * Your program only needs to support reading physical address, not writing
       to physical addresses.
+
 Address Translation
 ===
     * Again, translation will involve both a TLB and a page table.
@@ -32,9 +37,14 @@ Address Translation
       file addresses.txt, mask out the low order 16 bits, take bits[15:8] as the
       page number).
       2. Consult the TLB to translate the page number to a frame number.
-      If we have a TLB-hit, the TLB provides the frame number, the physical address = <frame_number , page offset>.
-      else, we have a TLB-miss, we consult the page-table for the frame number, again the physical address = <frame_number , page offset>.
-          If page table does not contain the frame number, a **page fault** has occurred. Thus the page is not in memory, it must be read in from the backing store into physical memory.
+      If we have a TLB-hit, the TLB provides the frame number, the physical
+      address = <frame_number , page offset>.
+      else, we have a TLB-miss, we consult the page-table for the frame number,
+      again the physical address = <frame_number , page offset>.
+      If page table does not contain the frame number, a **page fault** has
+      occurred. Thus the page is not in memory, it must be read in from the
+      backing store into physical memory.
+
 Handling Page Faults
 ===
     * The program will implement demand paging, i.e. a page is read into
@@ -67,11 +77,13 @@ Handling Page Faults
       strategy since the virtual address space size = the physical address space
       size, and all of physical address space is available for demand paging. In
       practice, part of physical memory must be used for the e.g. OS, and MMI/O.
+
 Test File
 ===
     * addresses.txt contains the logical addresses to be translated, one address
       per line. The byte value read from the corresponding physical address
       should be output as a signed char value.
+
 How to Begin
 ===
     * Start by obtaining the page number and page offset.
@@ -80,11 +92,13 @@ How to Begin
     things up, like normal the normal CPU caches.
     * Recall that the TLB only has 16 entries, so a replacement strategy will be
       requireD: use FIFO or LRU.
+
 How to Run Your Program
 ===
     * Example: `./a.out addresses.txt`
     * addresses.txt contains 1000 logical addresses.
     * The program should output 3 values as indicated in the file correct.txt.
+
 Statistics
 ===
     * The program should report:
@@ -96,6 +110,7 @@ Statistics
     * It is noted that addresses.txt was generated randomly, therefore do not
       expect a high TLB hit rate. In practice, real programs exhibit memory
       access locality.
+
 My Remarks
 ===
     * It is manifest that it is up to me to decide where in physical memory a
@@ -103,6 +118,7 @@ My Remarks
       simplest choice seems to be the next free page of physical memory, we can
       simple fill physical memory starting from lowest page number up the
       highest page number.
+
 Modifications
 ===
     * I will treat this as "part 2" of the project.
@@ -117,34 +133,177 @@ Modifications
 #include <stdio.h>
 #include <errno.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <limits.h>
 
-
-/*
-
-    Array of virtual addresses read from addresses.txt.
-    It is given that each address fits in a 16-bit integer and that there are
-    1000 of them.
-
+/*!
+    @defined
+    @discussion Given: Virtual address space size: 2^16 = 65536. Therefore the
+    max virtual address is 1 less than that.
 */
-#define NVADDRESSES 1000
-static short vaddrs[NVADDRESSES];
+#define MAX_VADDRESS ((unsigned long) ((1 << 16) - 1))
 
-/*
-Temporary string buffer for use with fgets() when reading addresses.txt line by
-line.
-
-// Temp.
-18446744073709551615 = 2**64 - 1
-+1 for sizeof('\n').
-//
-
-The minimum size for this buffer would be
-sizeof("65536") // 5 chars + 1 NULL terminator
-+1 for potential new line character after that. Total = 7 bytes.
-
+/*!
+    @defined NVADDRESSES
+    @discussion  It is given that each address fits in a 16-bit integer and that
+    there are 1000 of them.
 */
-static char t_addr_str[sizeof("65536") + 1];
+#define NVADDRESSES 1000U
 
+static unsigned short vaddrs[NVADDRESSES]; /* Array of virtual addresses read
+                                              from addresses.txt. It is given
+                                              that each address fits in a 16-bit
+                                              integer and that there are 1000 of
+                                              them. */
+
+/*!
+    @function init_vaddrs
+    @discussion Initializes the vaddrs array according to the contents of the
+    given filename
+    @param vaddrs_fname The filename of the file from which to read the virtual
+    addresses.
+    @result 0 if successful, otherwise error. The program should terminate.
+*/
+int init_vaddrs(const char *vaddrs_fname) {
+    FILE *vaddrs_fp = NULL;
+    size_t i = 0;
+    unsigned long t_vaddr = 0;
+    char *endptr = NULL;
+    char t_addr_str[sizeof("65536") + 1]; /* Temporary string buffer for use
+                                             with fgets() when reading
+                                             addresses.txt line by line.
+                                             The minimum size for this buffer is
+                                             sizeof("65536") = 5 digits + 1 NULL
+                                             terminator +1 for potential new
+                                             line character after that.
+                                             Total = 7 bytes. */
+
+    if (vaddrs_fname == NULL) {
+        assert(0);
+        return 1;
+    }
+
+    /* TEMP.
+       x Read file addresses.txt 1 line at a time.
+        For each line, if the line is not empty, and we haven't reached EOF,
+       x convert address to an unsigned integer,
+        take lower 16 bits,
+        print the page number and page offset. Compare page offset to
+        correct.txt.
+    */
+
+    errno = 0; // Per man page.
+    vaddrs_fp = fopen (vaddrs_fname, "r");
+
+    if (vaddrs_fp == NULL) {
+        printf("fopen(\"r\") returned NULL! filename = %s. error = %s.\n", \
+               vaddrs_fname, strerror(errno));
+        return 2;
+    }
+
+    errno = 0;
+    while (!feof(vaddrs_fp)) {
+
+        if(!fgets(t_addr_str, sizeof(t_addr_str), vaddrs_fp)) {
+            // EOF or error occurred.
+
+            /* FYI: If the file ends with a empty line then fgets() must be
+               called a number of times equal to the number of virtual addresses
+               in the file plus one in order to reach EOF. Hence this break.*/
+            printf("FYI: fgets() returned NULL.\n");
+            break;
+        }
+
+        printf("line # %lu.\n", i + 1);
+
+        if (*t_addr_str != '\0') {
+            if (t_addr_str[strlen(t_addr_str) - 1] == '\n')
+                printf("line is %s", t_addr_str);
+            else
+                printf("line is %s\n", t_addr_str);
+
+            // FYI: strtol() with base = 0 provides handling for hex and octal.
+            errno = 0;
+            t_vaddr = (unsigned long) strtol(t_addr_str, &endptr, 0);
+
+            if (t_addr_str == endptr) {
+                printf("strtol() found no digits at all! error = %s.\n", \
+                       strerror(errno));
+                return 3;
+            }
+
+            if (*endptr != '\n' && *endptr != '\0') {
+                printf("strtol() returned unexpected endptr!\n");
+                return 4;
+            }
+
+            if (errno != 0 && (t_vaddr == LONG_MAX || t_vaddr == LONG_MIN)) {
+                printf("strtol() overflowed or underflowed! error = %s.\n", strerror(errno));
+                return 5;
+            }
+
+            if (t_vaddr > MAX_VADDRESS) {
+                printf("Virtual address out of range (> %lu)! %lu = 0x%lX\n", MAX_VADDRESS, t_vaddr, t_vaddr);
+                return 6;
+            }
+
+            if (i >= NVADDRESSES) {
+                printf("Number of virtual addresses exceeds max allowed (%d).\n", NVADDRESSES);
+                return 7;
+            }
+
+            vaddrs[i] = (unsigned short) t_vaddr;
+            printf("Virtual address # %lu. %u = 0x%X\n", i, vaddrs[i], vaddrs[i]);
+            i++;
+
+        } else {
+            printf("Unexpected empty string!\n");
+            return 8;
+        }
+
+    }
+
+    if (feof(vaddrs_fp) && ferror(vaddrs_fp)) {
+        /* fgets() function does not distinguish between end-of-file and error,
+           and callers must use feof(3) and ferror(3) to determine which
+           occurred. */
+        printf("Got fgets() error! filename = %s. error = %s.\n", vaddrs_fname, strerror(errno));
+        return 9; // Bail. FYI: The OS will close the file.
+    }
+
+    errno = 0;
+    if (fclose(vaddrs_fp)) {
+        printf("fclose() returned error! filename = %s. error = %s.\n", vaddrs_fname, strerror(errno));
+        return 9;
+    }
+
+    return 0;
+}
+
+/*!
+    @function get_arg_vaddrs_filename
+    @discussion Gets the addresses.txt filename argument.
+    @param argc
+    @param argv
+    @param fname
+    @result 0 if successful. Error otherwise.
+*/
+int get_arg_vaddrs_filename (int argc, const char * const * const argv, const char ** const fname) {
+    const char *vaddrs_fname = "addresses.txt";
+
+    if (argc < 1 || argv == NULL || fname == NULL) {
+        assert(0);
+        return 1;
+    }
+
+    if (argc == 2) { // Use given filename instead of default.
+        vaddrs_fname = argv[1];
+    }
+
+    *fname = vaddrs_fname;
+
+    return 0;
+}
 /*!
 
     @function main
@@ -155,66 +314,41 @@ static char t_addr_str[sizeof("65536") + 1];
 
     @param argv
 
-    @result
+    @result 0 if successful.
 */
 int main (int argc, const char * const * const argv) {
-    FILE *vaddrs_fp;
-    const char *vaddrs_fname = "addresses.txt";
-    long t_vaddr;
-    size_t i = 0;
+    const char *fname = NULL;
 
+    if (argc < 1 || argv == NULL) {
+        assert(0);
+        return 1;
+    }
 
     assert(sizeof(short) == 2); // Verify the size of integers.
     assert(sizeof(int) == 4);
     assert(sizeof(long) == 8);
 
     if (argc != 1 && argc != 2) {
-        printf("Usage 1: ./a.out. The default input filename is addresses.txt.\n");
-        printf("Usage 2: ./a.out addresses.txt. addresses.txt can be any properly formatted filename.\n");
+        printf("Usage 1: ./a.out. The default input filename is \
+               addresses.txt.\n");
+        printf("Usage 2: ./a.out addresses.txt. addresses.txt can be any \
+                filename.\n");
         return 1;
     }
 
-    if (argc == 2) { // Use given filename instead of default.
-        vaddrs_fname = argv[1];
+    if (get_arg_vaddrs_filename (argc, argv, &fname)) {
+        return 2;
     }
 
-    printf("Input \"addresses\" filename is: \"%s\"\n", vaddrs_fname);
+    printf("Input \"addresses\" filename is: \"%s\"\n", fname);
 
     printf("OK...\n");
 
-    /*
-        Read file addresses.txt 1 line at a time.
-        For each line, if the line is not empty, and we haven't reached EOF,
-        convert address to an unsigned integer, take lower 16 bits,
-        print the page number and page offset. Compare page offset to
-        correct.txt.
-    */
-
-    errno = 0; // Per man page.
-    vaddrs_fp = fopen (vaddrs_fname, "r");
-
-    if (vaddrs_fp == NULL) {
-        printf("fopen(\"r\") returned NULL! filename = %s. error = %s.\n", vaddrs_fname, strerror(errno));
+    if (init_vaddrs(fname)) {
         return 3;
     }
 
-    clearerr(vaddrs_fp); // Not necessary at this point, just a precaution/reminder for later.
-    while (fgets(t_addr_str, sizeof(t_addr_str), vaddrs_fp) && !feof(vaddrs_fp)) {
-        printf("line %lu = %s", i, t_addr_str);
-        i++;
-    }
-
-    if (feof(vaddrs_fp) && ferror(vaddrs_fp)) {
-        printf("Got fgets() error! filename = %s. error = %s.\n", vaddrs_fname, strerror(errno));
-        return 4; // Bail. FYI: The OS will close the file.
-    }
-
-    errno = 0;
-    if (fclose(vaddrs_fp)) {
-        printf("fclose() returned error! filename = %s. error = %s.\n", vaddrs_fname, strerror(errno));
-        return 5;
-    }
-
     printf("Done.\n");
+
     return 0;
 }
