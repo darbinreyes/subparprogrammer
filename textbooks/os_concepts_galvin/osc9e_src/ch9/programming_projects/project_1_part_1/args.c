0d6352f22880aea4870f2a7cc3ff8730ec769b12
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-02-18 18:47:12 -0500

    Cleanup.

--- textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/args.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/args.c
@@ -1,16 +1,51 @@
+#include <stdio.h>
 #include <assert.h>
+#include "vm.h"
 
+/*!
+    @function valid_int_sizes
+    @discussion Verifies that the size of integers and virtual address
+    definitions are valid/as expected.
+    @result 0 if valid. Otherwise not valid.
+*/
+int valid_int_sizes(void) {
+    // Verify the size of integers.
+    if (sizeof(short) != 2 || sizeof(int) != 4 || sizeof(long) != 8) {
+        fprintf(stderr, "Unexpected int size.\n");
+        assert(0);
+        return 1;
+    }
+    /* This program only deals with virtual addresses less than 64-bits wide. */
+    if (!(ADDR_NBITS < sizeof(addr_t) * 8) || !(PAGE_OFFSET_NBITS < ADDR_NBITS)) {
+        fprintf(stderr, "Unsupported virtual address size/def.\n");
+        assert(0);
+        return 1;
+    }
+
+    return 0;
+}
+
+/*!
+    @function valid_args
+    @discussion Checks that the args passed to main() are valid.
+    @param argc The usual main() arg.
+    @param argv The usual main() arg.
+    @result 0 if valid. Otherwise not valid.
+*/
 int valid_args (int argc, const char * const * const argv) {
     if (argc < 1 || argv == NULL) {
         assert(0);
         return 1;
     }
 
-    if (argc != 1 && argc != 2) {
-        printf("Usage 1: ./a.out. The default input filename is "\
-               "addresses.txt.\n");
-        printf("Usage 2: ./a.out addresses.txt. addresses.txt can be any "\
+    if (argc <= 2) {
+        fprintf(stderr, \
+                "Usage 1: ./a.out. The default input filename is "\
+               "addresses.txt.\n"\
+                "Usage 2: ./a.out addresses.txt. addresses.txt can be any "\
                 "filename.\n");
         return 1;
     }
+
+    return 0;
 }
