284e5702971b812740b32f211c31602cf053d7cf
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-02-18 18:23:38 -0500

    Cleanup.

--- textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/project_1_part_1v1.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/project_1_part_1v1.c
@@ -135,43 +135,8 @@ Modifications
 #include <assert.h>
 #include <stdlib.h>
 #include <limits.h>
+#include "vm.h"
 
-/*!
-    @defined ADDR_NBITS
-    @discussion The number of bits in a virtual address.
-*/
-#define ADDR_NBITS (16U)
-
-/*!
-    @defined PAGE_OFFSET_NBITS
-    @discussion The number of bits used to represent a byte offset into a page.
-    This must be less than the number of bits in an address.
-
-    @TODO assert(PAGE_OFFSET_NBITS < ADDR_NBITS)
-*/
-#define PAGE_OFFSET_NBITS (8U)
-
-/*!
-    @defined ADDR_UINT_T
-    @discussion Unsigned integer type used to represent virtual addresses.
-*/
-#define ADDR_UINT_T unsigned long
-
-/*!
-    @typedef addr_t
-    @discussion Typedef for the unsigned integer type used to represent virtual
-    addresses.
-*/
-typedef ADDR_UINT_T addr_t;
-
-/*!
-    @defined MAX_V_ADDR
-    @discussion The max value of a virtual address is equal to the max unsigned
-    integer value that can be represented with the number of bits in a virtual
-    address.
-
-*/
-#define MAX_V_ADDR ((1UL << ADDR_NBITS) - 1UL)
 
 /*!
     @defined MAX_NUM_V_ADDRS
@@ -486,18 +451,6 @@ int main (int argc, const char * const * const argv) {
 */
 #define BACKING_STORE_SIZE V_MEM_SIZE
 
-typedef struct _pg_tbl_entry_t {
-    addr_t fn;          // frame number
-    unsigned char im:1; // 1 = the page is in memory at frame number fn, 0 = page fault, page is in backing store.
-/*
-    Thoughts on other fields - currently unused fields.
-    mode:3,    // UNIX style rwx permissions for this page.
-    cp_on_w:1, // 1 = This page is copy on write, 0 = not copy on write.
-    locked:1,  // 1 = This page is locked into memory by the OS, it cannot be kicked out/replaced.
-          :2;
-*/
-} pg_tbl_entry_t;
-
 /*!
     @defined PAGE_TABLE_LEN
     @discussion The length of the page table.
@@ -515,6 +468,30 @@ typedef struct _pg_tbl_entry_t {
     @discussion The size of the backing store divided by the size of a page.
 */
 #define NUM_BS_PAGES (BACKING_STORE_SIZE/PAGE_SIZE)
+
+/*!
+    @typedef pg_tbl_entry_t
+
+    @discussion Represents a page table entry.
+
+    @field fn Frame number.
+
+    @field im In Memory. 1 = the page is in memory at frame number fn, 0 = page
+    fault, page is in backing store.
+
+    @TODO
+    Thoughts on other fields - currently unused fields.
+    mode:3,    // UNIX style rwx permissions for this page.
+    cp_on_w:1, // 1 = This page is copy on write, 0 = not copy on write.
+    locked:1,  // 1 = This page is locked into memory by the OS, it cannot be
+                  kicked out/replaced.
+          :2;
+
+*/
+typedef struct _pg_tbl_entry_t {
+    addr_t fn;
+    unsigned char im:1;
+} pg_tbl_entry_t;
 
 /*!
     @function backing_store_read
