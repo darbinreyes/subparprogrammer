25fbc3c00b414dd2875c7015158c0a548138cf7f
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-03-04 17:15:06 -0500

    Finished implementing no_tlb_translate_v2p_addr().

--- textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/v2/project_1_part_1v2.c
+++ textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/v2/project_1_part_1v2.c
@@ -151,6 +151,7 @@
 #include "tlb.h"
 #include "pmem.h"
 
+int no_tlb_translate_v2p_addr(addr_t vaddr, addr_t *paddr);
 int translate_v2p_addr(addr_t vaddr, addr_t *paddr);
 int translate_all(void);
 
@@ -223,7 +224,14 @@ int translate_all(void) {
 
     for (i = 0; i < vaddrs_len; i++) {
         // Translate from virtual to physical address.
-        if(translate_v2p_addr(vaddrs[i], &paddr)) {
+
+        // if(translate_v2p_addr(vaddrs[i], &paddr)) {
+        //     assert(0);
+        //     return 1;
+        // }
+
+        // no_tlb_translate_v2p_addr
+        if(no_tlb_translate_v2p_addr(vaddrs[i], &paddr)) {
             assert(0);
             return 1;
         }
@@ -489,13 +497,105 @@ int translate_v2p_addr(addr_t vaddr, addr_t *paddr) {
     return 0;
 }
 
+int page_table_rm(addr_t frame_num) {
+    size_t i;
+
+    if (frame_num >= NUM_PAGE_FRAMES) {
+        assert(0);
+        return 1;
+    }
+
+    for (i = 0; i < PAGE_TABLE_LEN; i++) {
+        if (page_table[i].im && page_table[i].fn == frame_num) {
+            page_table[i].im = 0; // No longer in memory.
+            /* Mark with an invalid page number. */
+            page_table[i].fn = PAGE_TABLE_LEN;
+            break;
+        }
+    }
+
+    if (i >= PAGE_TABLE_LEN) {
+        /*! @discussion Unless there is an bug, the for loop above should
+        always be terminated by the break statement, not the test part of
+        the for loop, since by definition a page being evicted must
+        currently reside in memory, and hence have a corresponding valid
+        entry in the page table. If this page has an entry in the TLB
+        then tlb_rm() invalidates that entry. */
+        assert(0);
+        return 2;
+    }
+
+    return 0;
+}
+
+int page_table_add(addr_t page_num, addr_t frame_num) {
+    if (page_num >= PAGE_TABLE_LEN) {
+        assert(0);
+        return 1;
+    }
+
+    if (frame_num >= NUM_PAGE_FRAMES) {
+        assert(0);
+        return 2;
+    }
+
+    if(page_table[page_num].im) {
+        assert(0);
+        return 3;
+    }
+
+    page_table[page_num].fn = frame_num;
+    page_table[page_num].im = 1;
+    return 0;
+}
+
+/*!
+    @discussion When a page fault occurs and there are no free frames a page
+    must be replaced. This function performs a page replacement.
+*/
+int page_replace(addr_t page_num, addr_t *frame_num) {
+    static addr_t victim_frame = 0;
+
+    if (page_num >= PAGE_TABLE_LEN) {
+        assert(0);
+        return 1;
+    }
+
+    if (frame_num == NULL) {
+        assert(0);
+        return 1;
+    }
+
+    if (page_table_rm(victim_frame)) {
+        assert(0);
+        return 2;
+    }
+
+    if(backing_store_pg_in(page_num, p_mem_addr() + victim_frame * PAGE_SIZE)) {
+      assert(0);
+      return 3;
+    }
+
+    if(page_table_add(page_num, victim_frame)) {
+        assert(0);
+        return 4;
+    }
+
+    *frame_num = victim_frame;
+
+    //printf("Replaced page in frame number %lu\n", victim_frame);
+
+    victim_frame++;
+    victim_frame = victim_frame % NUM_PAGE_FRAMES;
+
+    return 0;
+}
+
+#define PHYSICAL_ADDR(fn, pgoff) ( ( (fn) << PAGE_OFFSET_NBITS ) | (pgoff) );
+
 int no_tlb_translate_v2p_addr(addr_t vaddr, addr_t *paddr) {
-    /*! @discussion The page frame number of the next free frame. When a page
-        fault occurs, the new page is read into physical memory at byte address
-        == free_framen * PAGE_SIZE. Unless there are no free frames, in which
-        case the new page is read into memory after evicting a page to make
-        room. */
-    static addr_t free_framen = 0;
+    /*! @discussion The page frame number of the next free frame. */
+    static addr_t free_frame = 0;
     addr_t page_offset, page_num, frame_num;
     size_t i;
 
@@ -513,15 +613,13 @@ int no_tlb_translate_v2p_addr(addr_t vaddr, addr_t *paddr) {
         Otherwise a page fault has occurred, i.e. the page is not in memory.
 
         If a page fault occurs and there is a free frame, read the page from the
-        backing store into a free frame, update the page table, and add a
-        corresponding TLB entry to the TLB. Concatenate the frame number and
-        page offset to form the physical address.
+        backing store into a free frame, and update the page table. Concatenate
+        the free frame number and page offset to form the physical address.
 
-        If a page fault occurs and there are no free frames, evict a page from
-        memory, read the page from the backing store into the newly freed frame,
-        update the page table, and add a corresponding TLB entry to the TLB.
-        Concatenate the frame number and page offset to form the physical
-        address.
+        If a page fault occurs and there are no free frames, replace a page
+        currently in memory with the page that generated the page fault.
+        Concatenate the frame number of the new page and page offset to form the
+        physical address.
     */
 
     if (paddr == NULL) {
@@ -537,121 +635,36 @@ int no_tlb_translate_v2p_addr(addr_t vaddr, addr_t *paddr) {
         return 1;
     }
 
-    if (in_tlb(page_num, &frame_num)) {
-        ntlb_hits++; // Statistics. TLB-hit
-    } else if (page_table[page_num].im) {
+    if (page_table[page_num].im) {
         // The page is in memory.
-        frame_num = page_table[page_num].fn;
-    } else {
-        /* Page fault! Get the page from the backing store, update page_table
-        and TLB. */
-        npf++; // Statistics
-        /*! @discussion
-          * Supporting physical memory smaller than virtual memory.
-          * Reduce size of physical mem. from 256 to 128 frames.
-          * if free_framen == NUM_PAGE_FRAMES
-              * no more free page frames
-              * a page must be evicted from memory to backing store
-              * since we are never writing to a page we can skip writing the
-                page from memory to the backing store.
+        *paddr = PHYSICAL_ADDR(page_table[page_num].fn, page_offset);
+        return 0;
+    }
 
-              * all we need to do is decide which page will be be replaced.
-              * FIFO replacement can be implemented using a circular array index.
-              * Once memory is full, page replacement will be determined
-                according to FIFO.
-              * After evicting a page, we have a free frame, and we proceed with
-                reading the into memory from the backing store and updating the
-                page table and TLB.
-        */
-        if (free_framen >= NUM_PAGE_FRAMES) {
-          // No free frame available, page replacement required.
-          //printf("Memory is full!\n");
-          if(evict_page(&frame_num)) {
-            assert(0);
-            return 1;
-          }
-
-          /*!
-              # Results
-
-              v0 statistics
-              N REFS 1000
-              N PAGE FAULTS 244 (%24.400000)
-              N TLB HITS 55 (%5.500000)
-
-              v1 statistics
-              N REFS 1000
-              N PAGE FAULTS 538 (%53.800000)
-              N TLB HITS 53 (%5.300000)
-
-              correct.txt
-              Virtual address: 48855 Physical address: 32983 Value: -75
-              v0 correct.txt
-              Virtual address: 48855 Physical address: 32983 Value: -75
-              v1 correct.txt
-              Virtual address: 48855 Physical address: 215 Value: -75
-
-              In v1, we expect a difference in at at least part of the physical
-              addresses since we have reduced the number of page frames.
-          */
-
-          for (i = 0; i < PAGE_TABLE_LEN; i++) {
-              /*!
-                  @discussion The page being evicted is the one residing in page
-                  frame number frame_num indicated by evict_page(). Since the page
-                  currently resides in memory there is a corresponding page table
-                  entry marked as valid. Find that page table entry and mark it as
-                  invalid.
-              */
-              if (page_table[i].im && page_table[i].fn == frame_num) {
-                  page_table[i].im = 0; // No longer in memory.
-                  /* Mark with an invalid page number. */
-                  page_table[i].fn = PAGE_TABLE_LEN;
-                  break;
-              }
-          }
-
-          if (i >= PAGE_TABLE_LEN || tlb_rm(i, frame_num)) {
-              /*! @discussion Unless there is an bug, the for loop above should
-              always be terminated by the break statement, not the test part of
-              the for loop, since by definition a page being evicted must
-              currently reside in memory, and hence have a corresponding valid
-              entry in the page table. If this page has an entry in the TLB
-              then tlb_rm() invalidates that entry. */
-              assert(0);
-              return 1;
-          }
-
-        } else {
-          // Use a free frame.
-          frame_num = free_framen;
-        }
-
-        if (frame_num >= NUM_PAGE_FRAMES) {
-            fprintf(stderr, "frame_num is out of bounds %lu >= %lu.\n", \
-                    frame_num, NUM_PAGE_FRAMES);
-            assert(0);
-            return 2;
-        }
-
+    /* Page fault! Must get the page from the backing store. */
+    npf++; // Statistics
+    if (free_frame < NUM_PAGE_FRAMES) {
+        // Use a free frame.
+        frame_num = free_frame;
         if(backing_store_pg_in(page_num, p_mem_addr() + frame_num * PAGE_SIZE)) {
+          assert(0);
+          return 3;
+        }
+
+        if(page_table_add(page_num, frame_num)) {
+          assert(0);
+          return 3;
+        }
+
+        free_frame++;
+    } else {
+        // No free frame available, page replacement required.
+        if (page_replace(page_num, &frame_num)) {
             assert(0);
             return 3;
         }
-
-        if (tlb_add(page_num, frame_num)) {
-            assert(0);
-            return 1;
-        }
-
-        page_table[page_num].fn = frame_num;
-        page_table[page_num].im = 1;
-        if (free_framen < NUM_PAGE_FRAMES)
-          free_framen++;
     }
 
-    assert(frame_num < NUM_PAGE_FRAMES);
-
-    *paddr = (frame_num << PAGE_OFFSET_NBITS) | page_offset;
+    *paddr = PHYSICAL_ADDR(frame_num, page_offset);
     return 0;
 }
