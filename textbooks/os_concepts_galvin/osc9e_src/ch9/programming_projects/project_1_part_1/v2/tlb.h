ab32b7205827a0a3bcd5e6e2cb724c5aadb07d99
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-03-11 15:58:46 -0500

    Progress towards TLB with list based FIFO replacement.

--- textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/v2/tlb.h
+++ textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/v2/tlb.h
@@ -1,6 +1,10 @@
 #ifndef __TLB_H__
 #define __TLB_H__
+
 int in_tlb(addr_t page_num, addr_t *frame_num);
 int tlb_add(addr_t page_num, addr_t frame_num);
 int tlb_rm(addr_t page_num, addr_t frame_num);
+
+extern size_t ntlb_hits;
+
 #endif
