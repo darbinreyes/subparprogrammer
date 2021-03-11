0c40dda2acb79d98adfbb791f987a5cc39c3112a
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-03-11 16:26:46 -0500

    Cleaning up.

--- textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/v2/pg_table.h
+++ textbooks/os_concepts_galvin/osc9e_src/ch9/programming_projects/project_1_part_1/v2/pg_table.h
@@ -2,7 +2,6 @@
 #define __PG_TABLE_H__
 
 int translate_v2p_addr(addr_t vaddr, addr_t *paddr);
-int no_tlb_translate_v2p_addr(addr_t vaddr, addr_t *paddr);
 void free_page_list(void);
 
 extern size_t npf;
