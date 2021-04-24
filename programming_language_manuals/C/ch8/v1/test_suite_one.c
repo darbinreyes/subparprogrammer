13d8fb4765003fcd06a284edbd4b9ef7115aa421
Darbin Emmanuel Reyes <1085688+darbinreyes@users.noreply.github.com>
2021-04-24 14:42:21 -0400

    Suprress sbrk() warnings. Finish seatest setup.

--- programming_language_manuals/C/ch8/v1/test_suite_one.c
+++ programming_language_manuals/C/ch8/v1/test_suite_one.c
@@ -1,4 +1,6 @@
 #include "seatest.h"
+#include "knr-malloc.h"
+#include "string.h"
 
 /* Given test cases.
 
@@ -12,7 +14,23 @@
 
 void test_0()
 {
+    #define STR0 "The lone and level sands stretch far away."
+    char *str = STR0;
+    int sz = sizeof (STR0);
+    char *p;
 
+    assert_true (0);
+    assert_true(p != NULL);
+
+    if (p == NULL) {
+        return;
+    }
+
+    memcpy(p, str, sz);
+
+    assert_string_equal(str, p);
+
+    knr_free(p);
 }
 
 void test_fixture_one( void )
